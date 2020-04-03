23ddb2e2e3d29428eda0ee7e9beef22c9642635e
daniarahmetov <daniar.ah55@gmail.com>
2020-04-03 21:23:21 +0600

    Исправил адрес ws. Расписал Dockerfile

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+if [ "$@" = '/usr/local/bin/apache2-foreground' ] \
+|| [ "$@" = '/var/www/html/vendor/bin/phpunit'  ]; then
+    /usr/local/bin/dockerize -timeout 60s -wait tcp://${DB_HOST}:3306
+    set -e
+    echo 'running migrations'
+    php artisan migrate --force
+    echo 'migrations done'
+    echo 'seeding database'
+    php artisan db:seed
+    echo 'seeds done'
+fi
+exec "$@"
