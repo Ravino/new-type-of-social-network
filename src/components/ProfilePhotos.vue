714bd8d7fe429ffa111d3a855c558ea697409a05
Alexey.Bashmakov <Alexey.Bashmakov@DigitalArrowTech.com>
2020-04-03 19:38:01 +0300

    + браузерный редирект с корня на /login

--- src/components/ProfilePhotos.vue
+++ src/components/ProfilePhotos.vue
@@ -1,8 +1,17 @@
 <template>
     <div id="profilePhotos" class="row plz-profile-photos mt-5">
-        <div class="col-12 bg-white-br20 py-3">
-            <div class="w-100 d-flex flex-row">
-                <div></div>
+        <div class="col-12 bg-white-br20 p-3">
+            <div class="">
+                <h6>Фотографии <span>{{user.photosNumber}}</span></h6>
+            </div>
+
+            <div class="">
+                <a href="#onmap">Показать на карте</a>
+            </div>
+            <div class="w-100 d-flex flex-row plz-profile-photos-list">
+                <div v-for="(photo, photoIndex) in photos" v-bind:key="postIndex" class="plz-profile-photo-item w-auto my-2 ml-0 mr-3">
+                    <img :src="photo.path" :alt="photoIndex" />
+                </div>
             </div>
         </div>
     </div>
@@ -12,6 +21,7 @@
 export default {
 name: 'ProfilePhotos',
 props: {
+    user: Object,
     photos: Array
 },
 data () {
@@ -26,5 +36,12 @@ methods: {
 </script>
 
 <style>
+.plz-profile-photos-list {
+    overflow-x: hidden;
+}
 
+/** класс для последней фотки **/
+.plz-profile-photos-list .plz-profile-photo-item:last-child {
+    border: 1px solid red;
+}
 </style>
