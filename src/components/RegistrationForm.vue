206d414af6493cda4bc62eea899559def437497a
Alexey.Bashmakov <Alexey.Bashmakov@DigitalArrowTech.com>
2020-04-03 11:23:14 +0300

    + общие стили в styles/App.css ~ изменения в форме регистрации

--- src/components/RegistrationForm.vue
+++ src/components/RegistrationForm.vue
@@ -1,72 +1,71 @@
 <template>
-    <div id="loginForm" class="card --bg-light --h-100">
-        <div class="card-body">
-            <div class="card-title text-center">
-                <h5>Быстрая регистрация</h5>
+    <div id="loginForm" class="">
+        <div class="text-center">
+            <h5>Быстрая регистрация</h5>
+        </div>
+
+        <form id="registrationForm" novalidate="novalidate">
+            <div class="form-group" :class="{ '--has-error': $v.model.firstName.$error, '--has-success': !$v.model.firstName.$invalid }">
+                <label for="firstName" class="d-none">Ваше имя</label>
+                <input v-model="model.firstName"
+                       :class="{ '--is-invalid': $v.model.firstName.$error, '--is-valid': !$v.model.firstName.$invalid }"
+                       @blur="$v.model.firstName.$touch()" @keydown="registrationKeyDownCheck($event)"
+                       type="text" class="lr-input form-control" id="firstName" ref="firstName" placeholder="Ваше имя" />
+
+                <div v-show="$v.model.firstName.$error" class="invalid-feedback">
+                    <p v-if="!$v.model.firstName.required" class="text-danger">Укажите как Вас зовут</p>
+                    <p v-if="!$v.model.firstName.minLength" class="text-danger">Врядли у Вас такое короткое имя?</p>
+                    <p v-if="!$v.model.firstName.maxLength" class="text-danger">Слишком длинное имя</p>
+                    <p v-if="!$v.model.firstName.isCorrectHumanName" class="text-danger">Только буквы в имени</p>
+                </div>
             </div>
 
-            <form id="registrationForm" novalidate="novalidate">
-                <div class="form-group" :class="{ 'has-error': $v.model.firstName.$error, 'has-success': !$v.model.firstName.$invalid }">
-                    <label for="firstName" class="d-none">Ваше имя</label>
-                    <input v-model="model.firstName"
-                           :class="{ 'is-invalid': $v.model.firstName.$error, 'is-valid': !$v.model.firstName.$invalid }"
-                           @blur="$v.model.firstName.$touch()" @keydown="registrationKeyDownCheck($event)"
-                           type="text" class="form-control" id="firstName" ref="firstName" placeholder="Ваше имя" />
+            <div class="form-group" :class="{ '--has-error': $v.model.lastName.$error, '--has-success': !$v.model.lastName.$invalid }">
+                <label for="lastName" class="d-none">Ваша фамилия</label>
+                <input v-model="model.lastName"
+                       :class="{ '--is-invalid': $v.model.lastName.$error, '--is-valid': !$v.model.lastName.$invalid }"
+                       @blur="$v.model.lastName.$touch()" @keydown="registrationKeyDownCheck($event)"
+                       type="text" class="lr-input form-control" id="lastName" ref="lastName" placeholder="Ваша фамилия" />
 
-                    <div v-show="$v.model.firstName.$error" class="invalid-feedback">
-                        <p v-if="!$v.model.firstName.required" class="text-danger">Укажите как Вас зовут</p>
-                        <p v-if="!$v.model.firstName.minLength" class="text-danger">Врядли у Вас такое короткое имя?</p>
-                        <p v-if="!$v.model.firstName.maxLength" class="text-danger">Слишком длинное имя</p>
-                        <p v-if="!$v.model.firstName.isCorrectHumanName" class="text-danger">Только буквы в имени</p>
-                    </div>
+                <div v-show="$v.model.lastName.$error" class="invalid-feedback">
+                    <p v-if="!$v.model.lastName.required" class="text-danger">Укажите свою фамилию</p>
+                    <p v-if="!$v.model.lastName.minLength" class="text-danger">Врядли у Вас такая короткая фамилия</p>
+                    <p v-if="!$v.model.lastName.maxLength" class="text-danger">Слишком длинная фамилия</p>
+                    <p v-if="!$v.model.lastName.isCorrectHumanName" class="text-danger">Только буквы в фамилии</p>
                 </div>
+            </div>
 
-                <div class="form-group" :class="{ 'has-error': $v.model.lastName.$error, 'has-success': !$v.model.lastName.$invalid }">
-                    <label for="lastName" class="d-none">Ваша фамилия</label>
-                    <input v-model="model.lastName"
-                           :class="{ 'is-invalid': $v.model.lastName.$error, 'is-valid': !$v.model.lastName.$invalid }"
-                           @blur="$v.model.lastName.$touch()" @keydown="registrationKeyDownCheck($event)"
-                           type="text" class="form-control" id="lastName" ref="lastName" placeholder="Ваша фамилия" />
+            <div class="form-group" :class="{ '--has-error': $v.model.email.$error, '--has-success': !$v.model.email.$invalid }">
+                <label for="userEmail" class="d-none">Ваш E-mail</label>
+                <input v-model="model.email"
+                       :class="{ '--is-invalid': $v.model.email.$error, '--is-valid': !$v.model.email.$invalid }"
+                       @blur="$v.model.email.$touch()" @keydown="registrationKeyDownCheck($event)"
+                       type="text" class="lr-input form-control" id="userEmail" placeholder="Ваш E-mail" />
 
-                    <div v-show="$v.model.lastName.$error" class="invalid-feedback">
-                        <p v-if="!$v.model.lastName.required" class="text-danger">Укажите свою фамилию</p>
-                        <p v-if="!$v.model.lastName.minLength" class="text-danger">Врядли у Вас такая короткая фамилия</p>
-                        <p v-if="!$v.model.lastName.maxLength" class="text-danger">Слишком длинная фамилия</p>
-                        <p v-if="!$v.model.lastName.isCorrectHumanName" class="text-danger">Только буквы в фамилии</p>
-                    </div>
+                <div v-show="$v.model.email.$error" class="invalid-feedback">
+                    <p v-if="!$v.model.email.required" class="text-danger">Укажите свой е-мейл</p>
+                    <p v-if="!$v.model.email.email" class="text-danger">Укажите корректный е-мейл</p>
                 </div>
+            </div>
 
-                <div class="form-group" :class="{ 'has-error': $v.model.email.$error, 'has-success': !$v.model.email.$invalid }">
-                    <label for="userEmail" class="d-none">Ваш E-mail</label>
-                    <input v-model="model.email"
-                           :class="{ 'is-invalid': $v.model.email.$error, 'is-valid': !$v.model.email.$invalid }"
-                           @blur="$v.model.email.$touch()" @keydown="registrationKeyDownCheck($event)"
-                           type="text" class="form-control" id="userEmail" placeholder="Ваш E-mail" />
+            <div class="form-group" :class="{ '--has-error': $v.model.birthDate.$error, '--has-success': !$v.model.birthDate.$invalid }">
+                <label for="userBirth" class="d-none">Дата рождения</label>
+                <input v-model="model.birthDate"
+                    type="text" class="lr-input form-control" id="userBirth" pattern="(0[1-9]|1[0-9]|2[0-9]|3[01]).(0[1-9]|1[012]).[0-9]{4}"
+                       :class="{ '--is-invalid': $v.model.birthDate.$error, '--is-valid': !$v.model.birthDate.$invalid }"
+                       @blur="$v.model.birthDate.$touch()" @keydown="birthDateKeyDown($event)"
+                       placeholder="Дата рождения" />
 
-                    <div v-show="$v.model.email.$error" class="invalid-feedback">
-                        <p v-if="!$v.model.email.required" class="text-danger">Укажите свой е-мейл</p>
-                        <p v-if="!$v.model.email.email" class="text-danger">Укажите корректный е-мейл</p>
-                    </div>
+                <div v-show="$v.model.birthDate.$error" class="invalid-feedback">
+                    <p v-if="!$v.model.birthDate.isValidDMY" class="text-danger">Укажите дату в формате ДД.MM.ГГГГ</p>
                 </div>
+            </div>
 
-                <div class="form-group" :class="{ 'has-error': $v.model.birthDate.$error, 'has-success': !$v.model.birthDate.$invalid }">
-                    <label for="userBirth" class="d-none">Дата рождения</label>
-                    <input v-model="model.birthDate"
-                        type="text" class="form-control" id="userBirth" pattern="(0[1-9]|1[0-9]|2[0-9]|3[01]).(0[1-9]|1[012]).[0-9]{4}"
-                           :class="{ 'is-invalid': $v.model.birthDate.$error, 'is-valid': !$v.model.birthDate.$invalid }"
-                           @blur="$v.model.birthDate.$touch()" @keydown="birthDateKeyDown($event)"
-                           placeholder="Дата рождения" />
-
-                    <div v-show="$v.model.birthDate.$error" class="invalid-feedback">
-                        <p v-if="!$v.model.birthDate.isValidDMY" class="text-danger">Укажите дату в формате ДД.MM.ГГГГ</p>
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <button id="btnRegistration" type="button" :disabled="$v.$invalid" @click="startRegistration()" class="btn btn-primary btn-block w-75 m-auto rounded-pill">Регистрация</button>
-                </div>
-            </form>
-        </div>
+            <div class="form-group">
+                <button id="btnRegistration" type="button" :disabled="$v.$invalid" @click="startRegistration()"
+                        class="btn plz-btn plz-btn-primary">Регистрация</button>
+            </div>
+        </form>
     </div>
 </template>
 
@@ -189,3 +188,6 @@ methods: {
 }
 </script>
 
+<style>
+
+</style>
--- src/components/RegistrationModal.vue
+++ src/components/RegistrationModal.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="modal show" tabindex="-1" role="dialog" aria-labelledby="Modal" aria-hidden="true" style="display: block; background-color: rgba(0, 0, 0, .7);" @click.stop="hideRegistrationModal">
-        <div class="modal-dialog modal-lg modal-dialog-centered" role="document" @click.stop="">
-            <div class="modal-content">
+        <div class="modal-dialog modal-dialog-centered" role="document" @click.stop="">
+            <div class="modal-content bg-white-br20">
 <!--                <div class="modal-header">-->
 <!--                    <h4 class="modal-title">Быстрая регистрация</h4>-->
 <!--                    <button type="button" class="close" data-dismiss="modal" aria-label="Close" @click="hideAlertModal">-->
