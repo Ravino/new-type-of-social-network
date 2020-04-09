6cc11e630bb5cfc6effc9eb68da9ae6af2055155
daniarahmetov <daniar.ah55@gmail.com>
2020-04-09 21:11:26 +0600

    Refactored profile fields. Fixed profile update

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -31,31 +31,8 @@ class Profile extends Model
     ];
 
     protected $fillable = [
-        'firstname', 'lastname', 'birthday', 'city', 'sex', 'relationship', 'relationship_user_id',
+        'first_name', 'last_name', 'birthday', 'city', 'sex', 'relationship', 'relationship_user_id',
     ];
-
-    //TODO: Please move this to request handler
-    public static function rules($keys = [])
-    {
-        $rules = [
-            'firstname' => 'required|string|min:1|max:255',
-            'lastname' => 'required|string|min:1|max:255',
-            'birthday' => 'date_format:Y-m-d|nullable',
-            'sex' => Rule::in(array_keys(self::SEX_VARIANTS)),
-            'city' => 'string|min:1|max:255',
-            'relationship' => Rule::exists('profile_relationships', 'id') . '|nullable',
-            'relationship_user_id' => 'nullable',
-        ];
-
-        if (count($keys)) {
-            return array_filter($rules, function ($index) use ($keys) {
-                return in_array($index, $keys);
-
-            }, ARRAY_FILTER_USE_KEY);
-        }
-
-        return $rules;
-    }
 
     public function getDateFormat()
     {
