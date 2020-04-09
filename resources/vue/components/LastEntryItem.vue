1f18fc3c9a96ea4b515f21047ec4ffeafc104f4f
Yusif Zourab <wordlearningpress@gmail.com>
2020-04-09 09:42:36 +0300

    Logged in when click on user on login page.

--- resources/vue/components/LastEntryItem.vue
+++ resources/vue/components/LastEntryItem.vue
@@ -1,26 +1,21 @@
 <template>
     <div class="plz-last-entry-item">
-        <img v-bind:src="entryItem.src" class="plz-last-entry-user-pic" :alt="entryItem.fullName" />
+        <img v-bind:src="entryItem.src" class="plz-last-entry-user-pic" :alt="entryItem.fullName"/>
         <div class="d-inline-block text-center mx-auto px-4 my-0">
-            <a href="#/profile" :class="{ 'text-black-50' : !entryItem.isUser, 'text-body': entryItem.isUser }" class="">{{entryItem.fullName}}</a>
+            <a href="#/profile" :class="{ 'text-black-50' : !entryItem.isUser, 'text-body': entryItem.isUser }"
+               class="">{{entryItem.fullName}}</a>
         </div>
     </div>
 </template>
 
 <script>
-export default {
-name: 'LastEntryItem',
-props: {
-    entryItem : Object
-},
-data () {
-    return {
+    export default {
+        name: 'LastEntryItem',
+        props: {
+            entryItem: Object
+        },
+        data() {
+            return {}
+        },
     }
-},
-
-methods: {
-}
-
-}
 </script>
-
--- resources/vue/pages/LoginPage.vue
+++ resources/vue/pages/LoginPage.vue
@@ -1,35 +1,41 @@
 <template>
     <div class="row">
         <div class="col-5 px-0">
-            <LoginForm></LoginForm>
+            <LoginForm ref="loginForm"></LoginForm>
         </div>
 
         <div class="col-7 pr-0 pb-0" style="padding-left: 24px;">
-            <LastEntries></LastEntries>
+            <LastEntries @logInWithEntryItem="logInWithEntryItem"></LastEntries>
             <PliziMobile></PliziMobile>
         </div>
     </div>
 </template>
 
 <script>
-import LoginForm from '../components/LoginForm.vue';
-import LastEntries from '../components/LastEntries.vue';
-import PliziMobile from '../components/PliziMobile.vue';
+    import LoginForm from '../components/LoginForm.vue';
+    import LastEntries from '../components/LastEntries.vue';
+    import PliziMobile from '../components/PliziMobile.vue';
 
-export default {
-name: 'LoginPage',
-components: {LoginForm, LastEntries, PliziMobile},
-data() {
-    return {
+    export default {
+        name: 'LoginPage',
+        components: {LoginForm, LastEntries, PliziMobile},
+        data() {
+            return {}
+        },
+        methods: {
+            logInWithEntryItem(entries) {
+                if (entries && entries.isUser) {
+                    this.$refs.loginForm.model.email = "user@mail.com";
+                    this.$refs.loginForm.model.password = "secret";
+                    this.$refs.loginForm.startLogin();
+                }
+            },
+        },
+        mounted() {
+            this.$root.$emit('afterSuccessLogout', {redirect: false});
+        }
 
     }
-},
-
-mounted() {
-    this.$root.$emit('afterSuccessLogout', { redirect: false });
-}
-
-}
 </script>
 
 <style lang="scss" src="../styles/LoginPage.scss"></style>
