ace7c76e5c3452f25945ad53eba524c84d30c945
Alexey.Bashmakov <Alexey.Bashmakov@DigitalArrowTech.com>
2020-04-10 11:32:31 +0300

    ~ front-end revitalization

--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -26,7 +26,7 @@ const routes = [
 ];
 
 const router = new VueRouter({
-    // mode: 'history', // @tga - временно
+    mode: 'history', // @tga - временно
     base: process.env.BASE_URL,
     routes: routes
 });
