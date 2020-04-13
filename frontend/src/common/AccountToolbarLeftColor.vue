d7d3908e586b81af9b30a6c8b99c296ebff72372
Sergey Veremiychuk <sergey.veremiychuk@kopentech.com>
2020-04-13 17:06:45 +0300

    Search right and left panel styling

--- frontend/src/common/AccountToolbarLeftColor.vue
+++ frontend/src/common/AccountToolbarLeftColor.vue
@@ -3,47 +3,47 @@
         <a class="add-new ml-auto mr-auto text-center mx-auto mb-4" href="#">
             <i class="fa fa-plus" aria-hidden="true"></i>
         </a>
-        <a class="d-inline-block text-center mx-auto mb-4" href="#">
+        <a class="d-block text-center mx-auto mb-4" href="#">
             <button class="btn-close"  aria-label="delete">
                 <i class="fa fa-plus" aria-hidden="true"></i>
             </button>
-            <img src="/images/left-colored-icons/icon-01.png" alt="" />
+            <img class="mx-auto" src="/images/left-colored-icons/icon-01.png" alt="" />
         </a>
-        <a class="d-inline-block text-center mx-auto mb-4" href="#">
+        <a class="d-block text-center mx-auto mb-4" href="#">
             <button class="btn-close"  aria-label="delete">
                 <i class="fa fa-plus" aria-hidden="true"></i>
             </button>
-            <img src="/images/left-colored-icons/icon-02.png" alt="" />
+            <img class="mx-auto" src="/images/left-colored-icons/icon-02.png" alt="" />
         </a>
-        <a class="d-inline-block text-center mx-auto mb-4" href="#">
+        <a class="d-block text-center mx-auto mb-4" href="#">
             <button class="btn-close"  aria-label="delete">
                 <i class="fa fa-plus" aria-hidden="true"></i>
             </button>
-            <img src="/images/left-colored-icons/icon-03.png" alt="" />
+            <img class="mx-auto" src="/images/left-colored-icons/icon-03.png" alt="" />
         </a>
-        <a class="d-inline-block text-center mx-auto mb-4" href="#">
+        <a class="d-block text-center mx-auto mb-4" href="#">
             <button class="btn-close"  aria-label="delete">
                 <i class="fa fa-plus" aria-hidden="true"></i>
             </button>
-            <img src="/images/left-colored-icons/icon-04.png" alt="" />
+            <img class="mx-auto" src="/images/left-colored-icons/icon-04.png" alt="" />
         </a>
-        <a class="d-inline-block text-center mx-auto mb-4" href="#">
+        <a class="d-block text-center mx-auto mb-4" href="#">
             <button class="btn-close"  aria-label="delete">
                 <i class="fa fa-plus" aria-hidden="true"></i>
             </button>
-            <img src="/images/left-colored-icons/icon-05.png" alt="" />
+            <img class="mx-auto" src="/images/left-colored-icons/icon-05.png" alt="" />
         </a>
-        <a class="d-inline-block text-center mx-auto mb-4" href="#">
+        <a class="d-block text-center mx-auto mb-4" href="#">
             <button class="btn-close"  aria-label="delete">
                 <i class="fa fa-plus" aria-hidden="true"></i>
             </button>
-            <img src="/images/left-colored-icons/icon-06.png" alt="" />
+            <img class="mx-auto" src="/images/left-colored-icons/icon-06.png" alt="" />
         </a>
-        <a class="d-inline-block text-center mx-auto mb-4" href="#">
+        <a class="d-block text-center mx-auto mb-4" href="#">
             <button class="btn-close"  aria-label="delete">
                 <i class="fa fa-plus" aria-hidden="true"></i>
             </button>
-            <img src="/images/left-colored-icons/icon-07.png" alt="" />
+            <img class="mx-auto" src="/images/left-colored-icons/icon-07.png" alt="" />
         </a>
 
     </div>
--- frontend/src/common/AccountToolbarRight.vue
+++ frontend/src/common/AccountToolbarRight.vue
@@ -1,37 +1,37 @@
 <template>
-    <div id="rightToolbar" class="bg-white-br20 py-2">
+    <div id="rightToolbar" class="bg-white-br20 py-2 overflow-hidden">
         <div class="d-block w-50 mb-1 mt-2 mx-auto text-center m">
             <i class="fas fa-chevron-left"></i>
         </div>
 
         <hr class="w-100" />
 
-        <a class="w-100 d-block text-center mx-auto mb-2" href="#">
-            <img class="" width="48" height="48" src="/images/chat/alexandra.png" alt="Alexandra" />
+        <a class="rightToolbar-user w-100 d-block text-center py-1" href="#">
+            <img class="d-block mx-auto"  src="/images/chat/alexandra.png" alt="Alexandra" />
         </a>
 
-        <a class="w-100 d-block text-center mx-auto mb-2" href="#">
-            <img class="" width="48" height="48" src="/images/chat/alina.png" alt="Alina" />
+        <a class="rightToolbar-user w-100 d-block text-center py-1" href="#">
+            <img class="d-block mx-auto"  src="/images/chat/alina.png" alt="Alina" />
         </a>
 
-        <a class="w-100 d-block text-center mx-auto mb-2" href="#">
-            <img class="" width="48" height="48" src="/images/chat/anna.png" alt="Anna" />
+        <a class="rightToolbar-user w-100 d-block text-center py-1" href="#">
+            <img class="d-block mx-auto"  src="/images/chat/anna.png" alt="Anna" />
         </a>
 
-        <a class="w-100 d-block text-center mx-auto mb-2" href="#">
-            <img class="" width="48" height="48" src="/images/chat/anna2.png" alt="Anna" />
+        <a class="rightToolbar-user w-100 d-block text-center py-1" href="#">
+            <img class="d-block mx-auto"  src="/images/chat/anna2.png" alt="Anna" />
         </a>
 
-        <a class="w-100 d-block text-center mx-auto mb-2" href="#">
-            <img class="" width="48" height="48" src="/images/chat/maria.png" alt="Maria" />
+        <a class="rightToolbar-user w-100 d-block text-center py-1" href="#">
+            <img class="d-block mx-auto"  src="/images/chat/maria.png" alt="Maria" />
         </a>
 
-        <a class="w-100 d-block text-center mx-auto mb-2" href="#">
-            <img class="" width="48" height="48" src="/images/chat/mariya2.png" alt="Masha" />
+        <a class="rightToolbar-user w-100 d-block text-center py-1" href="#">
+            <img class="d-block mx-auto" src="/images/chat/mariya2.png" alt="Masha" />
         </a>
 
-        <a class="w-100 d-block text-center mx-auto mb-2" href="#">
-            <img class="" width="48" height="48" src="/images/chat/malunder.png" alt="Malunder" />
+        <a class="rightToolbar-user w-100 d-block text-center py-1" href="#">
+            <img class="d-block mx-auto"  src="/images/chat/malunder.png" alt="Malunder" />
         </a>
 
     </div>
