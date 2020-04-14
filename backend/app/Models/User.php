44d242b1d4b479d8bbd27c535d434a8b8c0c8488
daniarahmetov <daniar.ah55@gmail.com>
2020-04-14 14:03:26 +0600

    swagger ui container last_activity_fix

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -25,7 +25,7 @@ class User extends Authenticatable implements JWTSubject
      * @var array
      */
     protected $fillable = [
-        'email', 'token'
+        'email', 'token', 'last_activity_dt'
     ];
 
     /**
