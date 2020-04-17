5752141999c522acd5f99896a964a29ec939b4e0
Sergey Veremiychuk <sergey.veremiychuk@kopentech.com>
2020-04-17 19:03:52 +0300

    FE/notification styling

--- frontend/src/components/InvitationItem.vue
+++ frontend/src/components/InvitationItem.vue
@@ -1,6 +1,6 @@
 <template>
     <li class="plizi-invitation-item-user --media m-0 px-4 py-2" :class="calcClazz()">
-        <div class="plizi-invitation-item d-flex col-12">
+        <div class="plizi-invitation-item d-flex ">
             <router-link :to="`/user-`+invitation.id" tag="div" class="plizi-invitation-item-pic mr-3">
                 <img class="plizi-invitation-item-img rounded-circle overflow-hidden" v-bind:src="invitation.userPic" v-bind:alt="invitation.fullName" />
                 <span v-if="invitation.isOnline" class="plizi-invitation-item-isonline" title="онлайн"></span>
@@ -8,14 +8,20 @@
             </router-link>
 
             <div class="plizi-invitation-item-body m-0 pr-5">
-                <div class="plizi-invitation-item-top d-flex align-items-end justify-content-between">
-                    <router-link :to="`/user-`+invitation.id" tag="h6" class="plizi-invitation-item-name my-0">
+                <div class="plizi-invitation-item-top d-flex align-items-start justify-content-between">
+                    <router-link :to="`/user-`+invitation.id" tag="h6" class="plizi-invitation-item-name my-0"
+                                 :title=" invitation.fullName ">
                         {{ invitation.fullName }}
                     </router-link>
                 </div>
 
+                <div class="plizi-invitation-item-top d-flex align-items-end justify-content-between">
+                    <!--                    {{invitation.notifMessage}}-->
+                    <p class="plizi-invitation-item-desc mb-1"> хочет к Вам в друзья</p>
+                </div>
+
                 <div class="plizi-invitation-item-body-bottom d-flex pr-5">
-                    <p class="plizi-invitation-item-desc p-0 my-0  d-inline ">
+                    <p class="plizi-invitation-item-subdesc p-0 my-0  d-inline-block ">
                         <time :datetime="invitation.lastActivity" class="">
                             {{ invitation.lastActivity | lastMessageTime }}
                         </time>
@@ -23,13 +29,13 @@
                 </div>
             </div>
 
-            <div class="align-self-end ml-auto">
+            <div class="align-self-center ml-auto">
                 <div class="btn-group">
-                    <button class="btn btn-primary btn-sm" @click="acceptInvitation()">
-                        <i class="fas fa-check"></i>&nbsp;Принять дружбу
+                    <button class="btn btn-success btn-sm rounded" @click="acceptInvitation()">
+                        <i class="fas fa-check"></i>&nbsp;Принять
                     </button>
-                    <button class="btn btn-warning btn-sm" @click="declineInvitation()">
-                        <i class="fas fa-stop"></i>&nbsp;Отказ
+                    <button class="btn btn-danger btn-sm ml-2 rounded" @click="declineInvitation()">
+                        <i class="fas fa-stop"></i>&nbsp;Отказать
                     </button>
                 </div>
             </div>
@@ -111,94 +117,4 @@ mounted(){
 }
 </script>
 
-<style lang="scss">
-$invitationUserPicSize: 48px;
-$invitationTimeColor: #9A9A9A;
-$invitationNameColor: #363636;
-$invitationOnlineColor: #9FCD48;
-$invitationOfflineColor: #CACAC9;
 
-
-.plizi-invitation-item-user {
-    transition: .4s;
-
-    .user-friend-desc {
-        overflow: hidden;
-        text-overflow: ellipsis;
-        white-space: nowrap;
-    }
-}
-
-.plizi-invitation-item {
-    &-pic {
-        display: inline-block;
-        position: relative;
-        height: $invitationUserPicSize;
-        width: $invitationUserPicSize;
-
-        img {
-            display: inline-block;
-            width: $invitationUserPicSize;
-            height: $invitationUserPicSize;
-        }
-    }
-
-    &-isonline {
-        display: block;
-        position: absolute;
-        width: 10px;
-        height: 10px;
-        right: 0px;
-        bottom: 0px;
-        background-color: $invitationOnlineColor;
-        border: 1px solid white;
-        border-radius: 100%;
-    }
-
-    &-isoffline {
-        display: block;
-        position: absolute;
-        width: 10px;
-        height: 10px;
-        right: 0px;
-        bottom: 0px;
-        background-color: $invitationOfflineColor;
-        border: 1px solid white;
-        border-radius: 100%;
-    }
-
-    &-body {
-        &-top {
-            time {
-                font-size: 11px;
-                color: $invitationTimeColor; // add color to var
-            }
-
-        }
-    }
-
-    &-name {
-        font-size: 13px;
-        font-weight: 600;
-        color: $invitationNameColor;
-        line-height: 20px;
-        cursor: pointer;
-
-        &:hover {
-            text-decoration: underline;
-            color: $invitationTimeColor;
-        }
-    }
-
-    &-desc {
-        font-size: 12px;
-        color: $invitationTimeColor;
-    }
-
-    &-details {
-        font-size: 12px;
-        color: $invitationNameColor;
-    }
-
-}
-</style>
