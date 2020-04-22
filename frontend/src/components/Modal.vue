5457319e35b8d20e8f501017c535fa7d52693ad5
Yusif Zourab <wordlearningpress@gmail.com>
2020-04-22 13:58:23 +0300

    Added password modal in security and one item in privacy blocks.

---
+++ frontend/src/components/Modal.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="modal fade" :id="modalId" tabindex="-1" role="dialog" :aria-labelledby="modalId"
+         aria-hidden="true">
+        <div class="modal-dialog"
+             :class="{'modal-dialog-centered': vertically}"
+             role="document">
+            <div class="modal-content">
+                <slot name="header"></slot>
+                <div class="modal-body">
+                    <slot></slot>
+                </div>
+
+                <slot name="footer"></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Modal",
+        props: {
+            modalId: {
+                type: String,
+                default: null,
+                required: true,
+            },
+            vertically: {
+                type: Boolean,
+                default: false,
+            },
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
