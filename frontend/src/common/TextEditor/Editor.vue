d65c637d854b66f7aa20eda63c6c8128cdf155ae
Sergey Veremiychuk <sergey.veremiychuk@kopentech.com>
2020-04-23 18:19:23 +0300

    Chat footer can change #chatbody height - script

--- frontend/src/common/TextEditor/Editor.vue
+++ frontend/src/common/TextEditor/Editor.vue
@@ -1,8 +1,7 @@
 <template>
-    <div class="editor">
+    <div class="editor" ref="getHeight">
         <editor-content class="editor-content"
                         :editor="editor"
-                        ref="editor"
                         @keydown.native="onEditorKeyDown"/>
         <span v-if="!isFocusedEditor"
               class="placeholder">
@@ -55,6 +54,8 @@ data() {
             onBlur: this.onBlur,
         }),
         isFocusedEditor: false,
+        editorH: 32,
+        editorChangedH: 0,
     }
 },
 
@@ -73,6 +74,25 @@ methods: {
         this.editor.setContent(currText);
     },
 
+    getСhatFooterStartHeight(){
+        let chatFooterHeight = document.getElementById('chatFooter').offsetHeight;
+        this.editorH = chatFooterHeight; // TODO пересчитать высоту chatMessagesBody при изменении высоты chatFooter
+    },
+
+    checkEditorHeight() {
+        const chatMessageBody = document.getElementById('chatMessagesBody');
+        let chatMessageBodyH = chatMessageBody.offsetHeight;
+        let getChatFooterChangedHeight = document.getElementById('chatFooter').offsetHeight;
+
+        if (this.editorH !== getChatFooterChangedHeight) {
+
+            let chatMessageBodyNewHeight = chatMessageBodyH - (getChatFooterChangedHeight - this.editorH);
+
+            chatMessageBody.style.height = chatMessageBodyNewHeight + 'px';
+            this.editorH = getChatFooterChangedHeight; // TODO проверить
+        }
+    },
+
     onEditorKeyDown(ev) {
         if (13 === ev.keyCode && ev.ctrlKey === true) {
             const editorText = this.editor.getHTML();
@@ -82,6 +102,9 @@ methods: {
             this.$emit('editorPost', { postText : editorText });
         }
 
+        //this.editorH = this.$refs.getHeight.offsetHeight;
+
+        this.checkEditorHeight();
     },
 
     onFocus(event) {
@@ -100,8 +123,11 @@ methods: {
         return this.editor.getHTML();
     }
 },
+    mounted() {
+        this.getСhatFooterStartHeight()
+    },
 
-beforeDestroy() {
+    beforeDestroy() {
     this.editor.destroy()
 }
 }
