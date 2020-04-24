dfc0c60b72347b884581d99b0e7efd9ed0a117da
Alexey.Bashmakov <Alexey.Bashmakov@DigitalArrowTech.com>
2020-04-24 12:49:21 +0300

    + display attachments in forwards chats messages

--- frontend/src/common/Chat/ChatMessageItemReplyContent.vue
+++ frontend/src/common/Chat/ChatMessageItemReplyContent.vue
@@ -1,25 +1,29 @@
 <template>
-    <div v-if="message.isReply" class="message-resend pl-3 ml-3 mt-3">
+    <div class="message-resend pl-3 ml-3 mt-3">
         <div class="message-user-data  d-flex align-items-center mb-2 ">
             <div class="media-pic border rounded-circle mr-3">
-                <img :src="message.replyOn.userPic" :alt="message.replyOn.firstName"/>
+                <img :src="replyOn.userPic" :alt="replyOn.firstName"/>
             </div>
             <div class="media-body">
-                <h6 class="chatHeader-title w-75 align-self-start mt-2 pb-0 mb-0 pull-left" style="line-height: 20px;">{{message.replyOn.firstName}}</h6>
-                <p class="chatHeader-subtitle p-0 mb-0 mt-1 w-100 d-block">{{message.replyOn.createdAt | lastMessageTime}}</p>
+                <h6 class="chatHeader-title w-75 align-self-start mt-2 pb-0 mb-0 pull-left" style="line-height: 20px;">{{replyOn.firstName}}</h6>
+                <p class="chatHeader-subtitle p-0 mb-0 mt-1 w-100 d-block">{{replyOn.createdAt | lastMessageTime}}</p>
             </div>
         </div>
-        <p v-html="message.replyOn.body"></p>
+        <p v-html="replyOn.body"></p>
+
+        <ReplyMessageItemAttachments v-bind:message="replyOn"></ReplyMessageItemAttachments>
     </div>
 </template>
 
 <script>
+import ReplyMessageItemAttachments from './ChatMessageItemAttachments.vue';
 import PliziMessage from '../../classes/PliziMessage.js';
 
 export default {
 name : 'ChatMessageItemReplyContent',
 props : {
-    message : PliziMessage
-}
+    replyOn : PliziMessage,
+},
+components: {ReplyMessageItemAttachments}
 }
 </script>
