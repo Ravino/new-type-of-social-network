9ed7ee2fabe8b9b1edb620b10af0f4b53d547cf7
daniarahmetov <daniar.ah55@gmail.com>
2020-04-26 15:24:22 +0600

    Change user id

--- backend/Domain/Pusher/Listeners/DestroyMessageNotification.php
+++ backend/Domain/Pusher/Listeners/DestroyMessageNotification.php
@@ -22,8 +22,6 @@ class DestroyMessageNotification implements ShouldQueue
         $idsOfUsers = $event->getUsersListIds();
         $body = $event->getMessage();
         foreach ($idsOfUsers as $user_id) {
-            $body['userId'] = $user_id->user_id;
-            \Log::debug($body);
             Pusher::sentDataToServer(['data' => $body, 'topic_id' => Pusher::channelForUser($user_id->user_id), 'event_type' => 'message.deleted']);
         }
     }
