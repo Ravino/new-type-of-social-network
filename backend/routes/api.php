d827bd1207cce1dce390eed8e37e1904a951d4b4
RSol <slava.rudnev@gmail.com>
2020-05-06 18:11:53 +0300

    PLZ-215. Add enpoint for image community header

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -87,6 +87,7 @@ Route::group(['middleware' => ['auth.jwt', 'track.activity']], function () {
         Route::get('{id}/unsubscribe', 'Api\CommunityController@unsubscribe');
         Route::get('{id}/members', 'Api\CommunityController@members');
         Route::post('attachments', 'Api\CommunityController@uploadAttachments');
+        Route::post('header', 'Api\CommunityController@uploadHeader');
     });
     Route::get('communities/{community_id}/posts', 'Api\PostController@communityPosts');
 
