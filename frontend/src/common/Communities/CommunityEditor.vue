8f7a044cfae9ec6d5a3337a6c934a72f45b8a89e
Yusif Zourab <wordlearningpress@gmail.com>
2020-05-07 16:25:48 +0300

    [Community page] Added ability for store posts in communities.

---
+++ frontend/src/common/Communities/CommunityEditor.vue
@@ -0,0 +1,73 @@
+<template>
+    <TextEditor :id="`communityEditor`"
+                :clazz="`row plz-text-editor mb-4 p-4 h-auto  align-items-start bg-white-br20`"
+                :editorPlaceholder="'Что у Вас нового?'"
+                :dropToDown="true"
+                @editorPost="onTextPost"
+                work-mode="post">
+    </TextEditor>
+</template>
+
+<script>
+    import ChatMixin from "../../mixins/ChatMixin";
+    import TextEditor from "../TextEditor";
+
+    export default {
+        name: "CommunityEditor",
+        components: {
+            TextEditor
+        },
+        props: {
+            communityId: {
+                type: Number,
+                default: null,
+            },
+        },
+        mixins: [ChatMixin],
+        methods: {
+            async onTextPost(evData){
+                let msg = evData.postText.trim();
+
+                if (msg !== '') {
+                    const brExample = `<br/>`;
+                    msg = msg.replace(/<p><\/p>/g, brExample);
+                    msg = this.killBrTrail(msg);
+
+                    if (msg !== '') {
+                        this.savePost( msg, evData.attachments );
+                    } else if (evData.attachments.length > 0) {
+                        this.savePost( '<p></p>', evData.attachments );
+                    }
+                } else {
+                    if (evData.attachments.length > 0) {
+                        this.savePost( '', evData.attachments );
+                    }
+                }
+            },
+            async savePost(text, attachments) {
+                let response;
+                let formData = {};
+
+                formData.body = text.trim();
+
+                if (attachments && attachments.length) {
+                    formData.attachmentIds = attachments;
+                }
+
+                try {
+                    response = await this.$root.$api.$post.storePostByCommunity(this.communityId, formData);
+                } catch (e) {
+                    console.warn(e.detailMessage);
+                }
+
+                if (response) {
+                    this.$emit('addNewPost', response);
+                }
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
