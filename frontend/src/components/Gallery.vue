676e813c82024e0c8b0363cfca663239da1c082c
Bohdan <47145765+Bohdan-Pronkurov@users.noreply.github.com>
2020-05-12 14:40:49 +0300

    PLZ-350 gallery

---
+++ frontend/src/components/Gallery.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="plz-gallery">
+
+    </div>
+</template>
+
+<script>
+ export default {
+  name: "Gallery",
+  props: {
+   photos: {
+    type: Array,
+   },
+  },
+  computed: {
+   getPhotosLinks() {
+    return
+   },
+  },
+ }
+</script>
