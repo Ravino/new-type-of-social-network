70012b585253cadf031c3f9f97546975b7f8263b
Yusif Zourab <wordlearningpress@gmail.com>
2020-05-14 11:45:54 +0300

    PLZ-435 Fixed errors after registration and invalid birthday in profile.

--- backend/app/Http/Requests/Post/Post.php
+++ backend/app/Http/Requests/Post/Post.php
@@ -25,7 +25,7 @@ class Post extends Request
     {
         return [
             'name' => 'nullable|string|max:255',
-            'body' => 'required|string|max:10000',
+            'body' => 'required|string|max:10050',
             'attachmentIds' => 'array'
         ];
     }
