b95b4d2cfe04505cecac17af799b8c4711607da5
RSol <slava.rudnev@gmail.com>
2020-05-15 10:25:57 +0300

    PLZ-458. Fix pagination for user community. Create new endpoint for managed communities

--- backend/app/Models/CommunityTheme.php
+++ backend/app/Models/CommunityTheme.php
@@ -5,9 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Spiritix\LadaCache\Database\LadaCacheTrait;
 
 class CommunityTheme extends Model
 {
+    use LadaCacheTrait;
 
     public function getDateFormat()
     {
