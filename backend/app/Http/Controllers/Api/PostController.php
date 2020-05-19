8a85b55c0bb1ba9161462948f2cbd74524d054ba
Yusif Zourab <wordlearningpress@gmail.com>
2020-05-19 16:44:30 +0300

    Fixed posts for other users.

--- backend/app/Http/Controllers/Api/PostController.php
+++ backend/app/Http/Controllers/Api/PostController.php
@@ -70,7 +70,7 @@ class PostController extends Controller
      */
     public function userPosts(Request $request, $id) {
         $user = User::find($id);
-        $posts = Post::getWithoutOldPosts($user, $request->query('limit'), $request->query('offset'));
+        $posts = Post::getWithoutOldPosts($user, $request->query('limit'), $request->query('offset'), true);
 
         return new PostCollection($posts);
     }
