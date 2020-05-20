8eeab10acaf0638df877c4c2d81aeeee26079f23
Bohdan <47145765+Bohdan-Pronkurov@users.noreply.github.com>
2020-05-20 10:39:07 +0300

    PLZ-350 gallery in attach message and chat

--- frontend/src/common/FavoriteFriendItem.vue
+++ frontend/src/common/FavoriteFriendItem.vue
@@ -31,7 +31,7 @@
             </span>
         </div>
 
-        <div v-if="isShowLinkedChat" class="plz-linked-chat-block mr-3 bg-white-br20 " :class="{ 'active-chat': chatWindowShown }">
+        <div v-if="isShowLinkedChat" class="plz-linked-chat-block mr-3 bg-white-br20 " :class="{ 'active-chat plz-favorit-z': chatWindowShown }">
             <!--TODO @TRG class="is-pinned" когда чат прибит-->
             <div id="chatMessagesWrapper" class="plz-linked-chat-body bg-light d-none d-lg-flex flex-column p-0"
                  :class="{'is-pinned' : 'когда_чат_прибит'}">
