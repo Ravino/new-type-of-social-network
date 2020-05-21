1da181440da2c703316ce5d0aa57586797d92fd2
Yusif Zourab <wordlearningpress@gmail.com>
2020-05-21 17:20:31 +0300

    PLZ-345 Updated reposts.

--- frontend/src/common/Post/Post.vue
+++ frontend/src/common/Post/Post.vue
@@ -58,117 +58,131 @@
                 </div>
             </div>
 
-            <div v-if="post.sharedFrom" class="sharedFrom pt-3 pl-3">
-                <div class="col-12 plz-post-item-header">
+            <template v-if="recursivePosts && recursivePosts.length">
+                <div v-for="(recursivePost, index) in recursivePosts"
+                     :key="index" class="sharedFrom pt-3 pl-3">
 
-                    <div class="post-news-item d-flex flex-row align-content-center shared  pb-2">
+                    <template v-if="recursivePost.body && recursivePost.attachments">
+                        <div class="col-12 plz-post-item-header">
 
-                        <router-link v-if="post.sharedFrom.user"  :to="{name: 'PersonalPage', params: {id: post.sharedFrom.user.id}}" class="post-poster-pic mr-3">
-                            <img :src="post.sharedFrom.posterPic" :alt="post.sharedFrom.posterName"/>
-                        </router-link>
-                        <router-link v-else  :to="{name: 'CommunityPage', params: {id: post.sharedFrom.community.id}}" class="post-poster-pic mr-3">
-                            <img :src="post.sharedFrom.posterPic" :alt="post.sharedFrom.posterName"/>
-                        </router-link>
+                            <div class="post-news-item d-flex flex-row align-content-center shared  pb-2">
 
-                        <div class="post-poster-name d-flex flex-column justify-content-center">
-                            <h6 class="post-poster-title mb-1">
-                                <!-- TODO: @TGA странно что мы нигде не выводим название поста-->
-
-                                <router-link v-if="post.sharedFrom.user" :to="{name: 'PersonalPage', params: {id: post.sharedFrom.user.id}}">
-                                    <b>{{post.sharedFrom.posterName}}</b>
+                                <router-link v-if="recursivePost.user"
+                                             :to="{name: 'PersonalPage', params: {id: recursivePost.user.id}}"
+                                             class="post-poster-pic mr-3">
+                                    <img :src="recursivePost.posterPic" :alt="recursivePost.posterName"/>
                                 </router-link>
-                                <router-link v-else :to="{name: 'CommunityPage', params: {id: post.sharedFrom.community.id}}">
-                                    <b>{{post.sharedFrom.posterName}}</b>
+                                <router-link v-else :to="{name: 'CommunityPage', params: {id: recursivePost.community.id}}"
+                                             class="post-poster-pic mr-3">
+                                    <img :src="recursivePost.posterPic" :alt="recursivePost.posterName"/>
                                 </router-link>
-                            </h6>
-                            <time :datetime="post.sharedFrom.createdAt" class="post-poster-time">
-                                {{ post.sharedFrom.createdAt | lastPostTime }}
-                            </time>
-                        </div>
-                    </div>
-                </div>
 
-                <div class="col-12 plz-post-item-body pt-2">
-                    <template v-if="sharedLivePreview && typeof sharedLivePreview === 'object'">
-                        <p v-if="sharedLivePreview.text"
-                           class="post-main-text shared mb-0"
-                           v-html="sharedLivePreview.text">
-                        </p>
+                                <div class="post-poster-name d-flex flex-column justify-content-center">
+                                    <h6 class="post-poster-title mb-1">
+                                        <!-- TODO: @TGA странно что мы нигде не выводим название поста-->
 
-                        <template v-if="sharedLivePreview.videoLinks">
-                            <div class="youtube-video-link d-flex justify-content-center">
-                                <p class="post-main-text  mt-2"
-                                   v-html="sharedLivePreview.videoLinks"
-                                   @click.stop="hasSharedYoutubeLinks ? openVideoModal() : null">
-                                </p>
-                                <button class="video__button" type="button" aria-label="Запустить видео">
-                                    <svg width="68" height="48" viewBox="0 0 68 48">
-                                        <path class="video__button-shape" d="M66.52,7.74c-0.78-2.93-2.49-5.41-5.42-6.19C55.79,.13,34,0,34,0S12.21,.13,6.9,1.55 C3.97,2.33,2.27,4.81,1.48,7.74C0.06,13.05,0,24,0,24s0.06,10.95,1.48,16.26c0.78,2.93,2.49,5.41,5.42,6.19 C12.21,47.87,34,48,34,48s21.79-0.13,27.1-1.55c2.93-0.78,4.64-3.26,5.42-6.19C67.94,34.95,68,24,68,24S67.94,13.05,66.52,7.74z"></path>
-                                        <path class="video__button-icon" d="M 45,24 27,14 27,34"></path>
-                                    </svg>
-                                </button>
+                                        <router-link v-if="recursivePost.user"
+                                                     :to="{name: 'PersonalPage', params: {id: recursivePost.user.id}}">
+                                            <b>{{recursivePost.posterName}}</b>
+                                        </router-link>
+                                        <router-link v-else
+                                                     :to="{name: 'CommunityPage', params: {id: recursivePost.community.id}}">
+                                            <b>{{recursivePost.posterName}}</b>
+                                        </router-link>
+                                    </h6>
+                                    <time :datetime="recursivePost.createdAt" class="post-poster-time">
+                                        {{ recursivePost.createdAt | lastPostTime }}
+                                    </time>
+                                </div>
                             </div>
-                        </template>
-                    </template>
+                        </div>
 
-                    <template v-else>
-                        <p v-if="post.sharedFrom.body"
-                           class="post-main-text shared mb-0"
-                           v-html="this.$options.filters.toBR(post.sharedFrom.body)"></p>
-                    </template>
-                </div>
+                        <div class="col-12 plz-post-item-body pt-2">
+                            <template v-if="recursivePost.livePreview && typeof recursivePost.livePreview === 'object'">
+                                <p v-if="recursivePost.livePreview.text"
+                                   class="post-main-text shared mb-0"
+                                   v-html="recursivePost.livePreview.text">
+                                </p>
 
-                <div class="col-12 plz-post-item-images">
-                    <div class="post-images shared">
-                        <Gallery :post="post" v-if="sharedImageAttachments.length > 0" :images="sharedImageAttachments"></Gallery>
-
-                        <template v-for="(postAttachment) in post.sharedFrom.attachments">
-                            <template v-if="!postAttachment.isImage">
-                                <AttachmentFile :attach="postAttachment"/>
+                                <template v-if="recursivePost.livePreview.videoLinks">
+                                    <div class="youtube-video-link d-flex justify-content-center">
+                                        <p class="post-main-text  mt-2"
+                                           v-html="recursivePost.livePreview.videoLinks"
+                                           @click.stop="recursivePost.livePreview ? openVideoModal() : null">
+                                        </p>
+                                        <button class="video__button" type="button" aria-label="Запустить видео">
+                                            <svg width="68" height="48" viewBox="0 0 68 48">
+                                                <path class="video__button-shape"
+                                                      d="M66.52,7.74c-0.78-2.93-2.49-5.41-5.42-6.19C55.79,.13,34,0,34,0S12.21,.13,6.9,1.55 C3.97,2.33,2.27,4.81,1.48,7.74C0.06,13.05,0,24,0,24s0.06,10.95,1.48,16.26c0.78,2.93,2.49,5.41,5.42,6.19 C12.21,47.87,34,48,34,48s21.79-0.13,27.1-1.55c2.93-0.78,4.64-3.26,5.42-6.19C67.94,34.95,68,24,68,24S67.94,13.05,66.52,7.74z"></path>
+                                                <path class="video__button-icon" d="M 45,24 27,14 27,34"></path>
+                                            </svg>
+                                        </button>
+                                    </div>
+                                </template>
                             </template>
-                        </template>
-                    </div>
+
+                            <template v-else>
+                                <p v-if="recursivePost.body"
+                                   class="post-main-text shared mb-0"
+                                   v-html="recursivePost.body"></p>
+                            </template>
+                        </div>
+
+                        <div class="col-12 plz-post-item-images">
+                            <div class="post-images shared">
+                                <Gallery :post="post" v-if="recursivePost.imageAttachments.length > 0" :images="recursivePost.imageAttachments"></Gallery>
+
+                                <template v-for="(postAttachment) in recursivePost.attachments">
+                                    <template v-if="!postAttachment.isImage">
+                                        <AttachmentFile :attach="postAttachment"/>
+                                    </template>
+                                </template>
+                            </div>
+                        </div>
+                    </template>
+
                 </div>
+            </template>
+
+            <div class="col-12 plz-post-item-body pb-2"
+                 :class="{'--px-2 --pt-2': recursivePosts.length, 'pt-4': !recursivePosts.length}">
+                <template v-if="livePreview && typeof livePreview === 'object'">
+                    <p v-if="livePreview.text"
+                       class="post-main-text mb-0"
+                       v-html="livePreview.text">
+                    </p>
+
+                    <template v-if="livePreview.videoLinks">
+                        <div class="youtube-video-link d-flex justify-content-center">
+                            <p class="post-main-text  mt-2"
+                               v-html="livePreview.videoLinks"
+                               @click.stop="hasYoutubeLinks ? openVideoModal(true) : null">
+                            </p>
+                            <button class="video__button" type="button" aria-label="Запустить видео">
+                                <IconYoutube/>
+                            </button>
+                        </div>
+                    </template>
+                </template>
+
+                <template v-else>
+                    <p v-if="post.body"
+                       class="post-main-text mb-2"
+                       v-html="this.$options.filters.toBR(post.body)"></p>
+                </template>
             </div>
 
-            <div class="col-12 plz-post-item-body pb-2" :class="{'--px-2 --pt-2': post.sharedFrom, 'pt-4': !post.sharedFrom}">
-                    <template v-if="livePreview && typeof livePreview === 'object'">
-                        <p v-if="livePreview.text"
-                           class="post-main-text mb-0"
-                           v-html="livePreview.text">
-                        </p>
-
-                        <template v-if="livePreview.videoLinks">
-                            <div class="youtube-video-link d-flex justify-content-center">
-                                <p class="post-main-text  mt-2"
-                                   v-html="livePreview.videoLinks"
-                                   @click.stop="hasYoutubeLinks ? openVideoModal(true) : null">
-                                </p>
-                                <button class="video__button" type="button" aria-label="Запустить видео">
-                                    <IconYoutube/>
-                                </button>
-                            </div>
-                        </template>
-                    </template>
-
-                    <template v-else>
-                        <p v-if="post.body"
-                           class="post-main-text mb-2"
-                           v-html="this.$options.filters.toBR(post.body)"></p>
-                    </template>
-                </div>
-
             <div class="col-12 plz-post-item-images">
-                        <div class="post-images">
-                            <Gallery :post="post" v-if="imageAttachments.length > 0" :images="imageAttachments"></Gallery>
+                <div class="post-images">
+                    <Gallery :post="post" v-if="imageAttachments.length > 0" :images="imageAttachments"></Gallery>
 
-                        <template v-for="(postAttachment) in post.attachments">
-                            <template v-if="!postAttachment.isImage">
-                                <AttachmentFile :attach="postAttachment"/>
-                            </template>
+                    <template v-for="(postAttachment) in post.attachments">
+                        <template v-if="!postAttachment.isImage">
+                            <AttachmentFile :attach="postAttachment"/>
                         </template>
-                    </div>
+                    </template>
                 </div>
+            </div>
 
             <div class="plz-post-item-footer col-12 pt-4">
                 <div class="d-flex">
@@ -187,16 +201,16 @@
                                     </b>
                                     {{ post.likes }} пользователям
                                 </p>
-                               <p class="d-flex">
-                                   <router-link v-for="(user, index) in shortUsersLikes"
-                                                :key="index"
-                                                :to="{ name: 'PersonalPage', params: { id: user.id } }">
-                                       <img :src="user.profile.userPic"
-                                            :alt="user.profile.firstName + ' ' + user.profile.lastName"
-                                            :title="user.profile.firstName + ' ' + user.profile.lastName"
-                                            class="rounded-circle">
-                                   </router-link>
-                               </p>
+                                <p class="d-flex">
+                                    <router-link v-for="(user, index) in shortUsersLikes"
+                                                 :key="index"
+                                                 :to="{ name: 'PersonalPage', params: { id: user.id } }">
+                                        <img :src="user.profile.userPic"
+                                             :alt="user.profile.firstName + ' ' + user.profile.lastName"
+                                             :title="user.profile.firstName + ' ' + user.profile.lastName"
+                                             class="rounded-circle">
+                                    </router-link>
+                                </p>
                             </div>
                         </div>
                         <div class="post-watched-counter ml-4">
@@ -273,17 +287,13 @@
             },
         },
         mixins: [LinkMixin],
+        data() {
+            return {
+                recursivePostsSimple: [],
+                recursivePosts: [],
+            }
+        },
         computed: {
-            sharedLivePreview() {
-                let str = this.post.sharedFrom.body.replace(/<\/?[^>]+>/g, '').trim();
-
-                return this.transformStrWithLinks(str);
-            },
-            hasSharedYoutubeLinks() {
-                let str = this.post.sharedFrom.body.replace(/<\/?[^>]+>/g, '').trim();
-
-                return this.detectYoutubeLinks(str);
-            },
             hasYoutubeLinks() {
                 let str = this.post.body.replace(/<\/?[^>]+>/g, '').trim();
 
@@ -303,9 +313,6 @@
             },
             imageAttachments() {
                 return this.post.attachments.filter(attachment => attachment.isImage);
-            },
-            sharedImageAttachments() {
-                return this.post.sharedFrom.attachments.filter(attachment => attachment.isImage);
             },
             user() {
                 return this.$root.$auth.user;
@@ -328,17 +335,63 @@
                     videoLink,
                 })
             },
+            recursiveParent(post) {
+                if (!post) return;
+
+                if (this.post.id === post.id) {
+                    this.recursiveParent(post.sharedFrom);
+                    return;
+                }
+
+                if (!post.sharedFrom) {
+                    this.recursivePostsSimple.push(post);
+                    this.transformRecursivePosts();
+                } else {
+                    this.recursivePostsSimple.push(post);
+                    this.recursiveParent(post.sharedFrom);
+                }
+            },
+            recursiveLivePreview(str) {
+                str = str.replace(/<\/?[^>]+>/g, '').trim();
+
+                return this.transformStrWithLinks(str);
+            },
+            recursiveHasYoutubeLinks(str) {
+                str = str.replace(/<\/?[^>]+>/g, '').trim();
+
+                return this.detectYoutubeLinks(str);
+            },
+            recursiveImageAttachments(attachments) {
+                return attachments.filter(attach => attach.isImage);
+            },
+            transformRecursivePosts() {
+                this.recursivePostsSimple.forEach((recursivePost) => {
+                    this.recursivePosts.push({
+                        id: recursivePost.id,
+                        body: recursivePost.body,
+                        user: recursivePost.user,
+                        posterPic: recursivePost.posterPic,
+                        posterName: recursivePost.posterName,
+                        community: recursivePost.community,
+                        createdAt: recursivePost.createdAt,
+                        livePreview: this.recursiveLivePreview(recursivePost.body),
+                        hasYoutubeLinks: this.recursiveHasYoutubeLinks(recursivePost.body),
+                        imageAttachments: this.recursiveImageAttachments(recursivePost.attachments),
+                        attachments: recursivePost.attachments,
+                    });
+                });
+            },
 
             async onLike() {
                 let response = null;
 
-                try{
+                try {
                     response = await this.$root.$api.$post.likePost(this.post.id);
-                } catch (e){
-                    console.warn( e.detailMessage );
+                } catch (e) {
+                    console.warn(e.detailMessage);
                 }
 
-                if ( response !== null ){
+                if (response !== null) {
                     if (this.post.alreadyLiked) {
                         this.post.alreadyLiked = false;
                         this.post.likes--;
@@ -353,6 +406,11 @@
                     }
                 }
             },
+        },
+        mounted() {
+            if (this.post) {
+                this.recursiveParent(this.post);
+            }
         },
     }
 </script>
