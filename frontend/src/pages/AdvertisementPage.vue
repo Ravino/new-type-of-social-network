ba9a72b407990eba794e39cc6ac3dc2117c52369
Julia Ivanova <jeivano91@gmail.com>
2020-05-22 21:02:20 +0300

    PLZ-636 created pages for footer links

---
+++ frontend/src/pages/AdvertisementPage.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="about-page">
+        <div class="d-flex-row px-0 mb-lg-0 ml-2">
+            <h1 class="text-center">Реклама</h1>
+            <p class="text-justify">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam et lorem quis mauris lacinia consequat
+                vel ac tellus. Sed id molestie magna. Maecenas eget nisi sed orci rutrum euismod ut a metus. Nunc
+                finibus nibh sed varius fermentum. Aliquam tincidunt, ipsum quis luctus maximus, arcu mi lobortis felis,
+                mattis pretium leo nunc eget sem. Phasellus rutrum fringilla tempor. Etiam dapibus a velit vitae
+                consequat.
+            </p>
+            <p class="text-justify">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam et lorem quis mauris lacinia consequat
+                vel ac tellus. Sed id molestie magna. Maecenas eget nisi sed orci rutrum euismod ut a metus. Nunc
+                finibus nibh sed varius fermentum. Aliquam tincidunt, ipsum quis luctus maximus, arcu mi lobortis felis,
+                mattis pretium leo nunc eget sem. Phasellus rutrum fringilla tempor. Etiam dapibus a velit vitae
+                consequat.
+            </p>
+            <p class="text-justify">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam et lorem quis mauris lacinia consequat
+                vel ac tellus. Sed id molestie magna. Maecenas eget nisi sed orci rutrum euismod ut a metus. Nunc
+                finibus nibh sed varius fermentum. Aliquam tincidunt, ipsum quis luctus maximus, arcu mi lobortis felis,
+                mattis pretium leo nunc eget sem. Phasellus rutrum fringilla tempor. Etiam dapibus a velit vitae
+                consequat.
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: "AdvertisementPage",
+    }
+</script>
