a822900c184eac552f80d7231210f11a548ac040
daniarahmetov <daniar.ah55@gmail.com>
2020-05-25 14:12:42 +0600

    PLZ-430 Remove chats with zero attendees

--- backend/app/Models/Post.php
+++ backend/app/Models/Post.php
@@ -111,47 +111,37 @@ class Post extends Model
             ->get()
             ->toArray();
 
-        $communitiesPosts = collect();
-
-        foreach($communities as $community) {
-            $communitiesPosts->push(Post::where('postable_type', Community::class)
-                ->where('postable_id', $community->id)
-                ->where('created_at', '>', Carbon::parse($community->pivot->created_at)->timestamp)
-                ->pluck('id'));
-        }
-
-        $friendsPosts = collect();
+        $posts = self::with(['attachments', 'postable', 'author', 'usersLikes' => function ($query) {
+            return $query->limit(8)->get();
+        }, 'parent' => function ($query) {
+            return $query->withTrashed()->get();
+        }]);
 
         foreach($friends as $friend) {
             if ($friend->status) {
                 if ($friend->sender_id !== $user->id) {
-                    $friendsPosts->push(Post::where('postable_type', User::class)
+                    $posts->orWhere('postable_type', User::class)
                         ->where('postable_id', $friend->sender_id)
-                        ->where('created_at', '>', Carbon::parse($friend->created_at)->timestamp)
-                        ->pluck('id'));
+                        ->where('created_at', '>', Carbon::parse($friend->created_at)->timestamp);
                 }
 
                 if ($friend->recipient_id !== $user->id) {
-                    $friendsPosts->push(Post::where('postable_type', User::class)
+                    $posts->orWhere('postable_type', User::class)
                         ->where('postable_id', $friend->recipient_id)
-                        ->where('created_at', '>', Carbon::parse($friend->created_at)->timestamp)
-                        ->pluck('id'));
+                        ->where('created_at', '>', Carbon::parse($friend->created_at)->timestamp);
                 }
             }
         }
 
-        $communitiesPosts = $communitiesPosts->collapse();
-        $friendsPosts = $friendsPosts->collapse();
-        $userPosts = $user->posts()->pluck('id');
-        $postsIds = $communitiesPosts->merge($friendsPosts);
-        $postsIds = $postsIds->merge($userPosts);
+        foreach($communities as $community) {
+            $posts->orWhere('postable_type', Community::class)
+                ->where('postable_id', $community->id)
+                ->where('created_at', '>', Carbon::parse($community->pivot->created_at)->timestamp);
+        }
+        $posts->orWhere('postable_type', User::class)
+            ->where('postable_id', \Auth::user()->id);
 
-        return Post::whereIn('id', $postsIds)
-            ->with(['postable', 'author', 'usersLikes' => function ($query) {
-                return $query->limit(8)->get();
-            }, 'parent' => function ($query) {
-                return $query->withTrashed()->get();
-            }])
+        return $posts
             ->limit($limit ?? 50)
             ->offset($offset ?? 0)
             ->orderBy('id', 'desc')
