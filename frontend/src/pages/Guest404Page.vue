e5629fdb320a4e3f15340b3d7379efe7da8860aa
Julia Ivanova <jeivano91@gmail.com>
2020-06-01 15:22:16 +0300

    PLZ-852 changed size

--- frontend/src/pages/Guest404Page.vue
+++ frontend/src/pages/Guest404Page.vue
@@ -1,11 +1,12 @@
 <template>
     <div class="error-page d-flex">
-                    <div class="plz-post-item mb-4 mx-auto bg-white-br20 p-4">
+                    <div class="plz-post-item mb-4 mx-auto bg-white-br20 w-100 p-4">
                         <div class="error-image mx-auto w-50">
                             <img src="../images/ErrorImage.svg">
                         </div>
                         <div class="alert text-center">
-                            Извините! Страница, которую Вы ищете, не может быть найдена.
+                            Извините! <br />
+                            Страница, которую Вы ищете, не может быть найдена.
                         </div>
                     </div>
                 </div>
