89b36c2792f999ea093603a0d12017dd8066b5db
daniarahmetov <daniar.ah55@gmail.com>
2020-06-11 00:29:22 +0600

    Nginx update

--- backend/docker/queue/docker-entrypoint.sh
+++ backend/docker/queue/docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 php artisan rabbitmq:queue-declare default
-while :; do echo "reconnecting"; php artisan queue:work --delay=1 --sleep=1 --tries=3; done
+while :; do echo "reconnecting"; php artisan rabbitmq:consume --tries=3; done
