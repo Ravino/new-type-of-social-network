65c6c0eb251abec962688049e669697e24e04e11
Ruslan Valis <ruslan@valis.team>
2020-07-26 10:18:59 +0300

    Add versions for docker entrypoints for back containers

--- backend/docker/back-api/docker-entrypoint.sh
+++ backend/docker/back-api/docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-php artisan optimize
+app_version=git log --pretty="%H" -n1 HEAD
+echo "[API] Version: ${app_version}"
+
 php artisan migrate --force
 php artisan db:seed --force
 exec php artisan swoole:http start
--- backend/docker/back-queue-worker/docker-entrypoint.sh
+++ backend/docker/back-queue-worker/docker-entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-php artisan optimize
+app_version=git log --pretty="%H" -n1 HEAD
+echo "[QueueWorker] Version: ${app_version}"
+
 exec php artisan queue:work redis --queue=high,default
