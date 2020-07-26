cdfd28473d06b487022cdbaa66e094cb3e1e5d21
Ruslan Valis <ruslan@valis.team>
2020-07-26 09:41:34 +0300

    No cache for frontend

--- update-frontend.sh
+++ update-frontend.sh
@@ -4,7 +4,7 @@ echo "START Update frontend docker image @ "`date`
 
 cd frontend/
 aws ecr get-login-password --region eu-central-1 | sudo docker login --username AWS --password-stdin 884088487044.dkr.ecr.eu-central-1.amazonaws.com
-sudo docker build -t eu-central-1-test-plizi-front-nginx:latest -f docker/front-nginx/Dockerfile .
+sudo docker build --no-cache -t eu-central-1-test-plizi-front-nginx:latest -f docker/front-nginx/Dockerfile .
 sudo docker tag eu-central-1-test-plizi-front-nginx:latest 884088487044.dkr.ecr.eu-central-1.amazonaws.com/eu-central-1-test-plizi-front-nginx:latest
 sudo docker push 884088487044.dkr.ecr.eu-central-1.amazonaws.com/eu-central-1-test-plizi-front-nginx:latest
 
