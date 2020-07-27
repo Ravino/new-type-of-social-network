4d81d864546cacd20f8c3eb62c934e85ef2494f5
Ruslan Valis <ruslan@valis.team>
2020-07-28 02:08:48 +0300

    Fix validate

--- environment/app.sh
+++ environment/app.sh
@@ -80,10 +80,10 @@ function validate-one() {
     fi;
 }
 function validate() {
-    cd "../templates/"
+    cd "templates/"
     for TEMPLATE in $(find . -name '*.yml'); do
         # Validate the template with CloudFormation
-        ERRORS=$(aws cloudformation validate-template --template-body file://../templates/$TEMPLATE 2>&1 >/dev/null);
+        ERRORS=$(aws cloudformation validate-template --template-body file://$TEMPLATE 2>&1 >/dev/null);
         if [ "$?" -gt "0" ]; then
             ((ERROR_COUNT++));
             echo "FAILED $TEMPLATE: $ERRORS";
