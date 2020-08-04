83feb544ea23076605270146a65baf5f7ccf2f29
Ruslan Valis <ruslan@valis.team>
2020-08-04 21:58:17 +0300

    Fix update-backend.sh (laravel-docker rebuild) script

--- update-backend.sh
+++ update-backend.sh
@@ -4,9 +4,9 @@ echo "START Update frontend docker image @ "`date`
 
 cd backend/
 aws ecr get-login-password --region eu-central-1 | sudo docker login --username AWS --password-stdin 884088487044.dkr.ecr.eu-central-1.amazonaws.com
-sudo docker build -t laravel-docker:latest -f docker/Dockerfile .
-sudo docker tag laravel-docker:latest 884088487044.dkr.ecr.eu-central-1.amazonaws.com/laravel-docker:latest
-sudo docker push 884088487044.dkr.ecr.eu-central-1.amazonaws.com/laravel-docker:latest
+sudo docker build --no-cache -t eu-central-1-test-plizi-laravel-docker:latest -f docker/laravel/Dockerfile .
+sudo docker tag eu-central-1-test-plizi-laravel-docker:latest 884088487044.dkr.ecr.eu-central-1.amazonaws.com/eu-central-1-test-plizi-laravel-docker:latest
+sudo docker push 884088487044.dkr.ecr.eu-central-1.amazonaws.com/eu-central-1-test-plizi-laravel-docker:latest
 
 echo "FINISH Update frontend docker image @ "`date`
 
