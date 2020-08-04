bc609d99a8ebe4d1ed88fb4580f18bcd3f8be951
Ruslan Valis <ruslan@valis.team>
2020-08-04 21:48:45 +0300

    Disable supervisor usage on back-queue-worker

--- backend/docker/back-queue-worker/docker-entrypoint.sh
+++ backend/docker/back-queue-worker/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# exec php artisan queue:work --quiet --queue=redis2,redis
-echo "starting..."
-pgrep -fl supervisor
-exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
+exec php artisan queue:work --quiet --queue=redis2,redis
+#echo "starting..."
+#pgrep -fl supervisor
+#exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
