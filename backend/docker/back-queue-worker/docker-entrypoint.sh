76a1023e683166d1e445041609a749fd9d901b04
Ruslan Valis <ruslan@valis.team>
2020-08-05 09:35:46 +0300

    Add supervisor

--- backend/docker/back-queue-worker/docker-entrypoint.sh
+++ backend/docker/back-queue-worker/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-exec php artisan queue:work --queue=redis2,redis
+#exec php artisan queue:work --queue=redis2,redis
 #exec php artisan queue:work --quiet --queue=redis2,redis
-#echo "starting..."
-#pgrep -fl supervisor
-#exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
+echo "Starting..."
+pgrep -fl supervisor
+exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
