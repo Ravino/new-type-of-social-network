59acdce0154f4d9fead807f4ec934c7eff6c70b4
Ruslan Valis <ruslan@valis.team>
2020-08-09 19:03:39 +0300

    (FrontNginx) Update Dockerfile for multistage build process

--- update-frontend.sh
+++ update-frontend.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
-echo "START Update frontend docker image @ "`date`
+set -e
+
+BUILD_ONLY=${1:-""}
+IMAGE_NAME=${2:-"eu-central-1-test-plizi-front-nginx"}
+BUILD_FILE_PATH=${4:-"docker/front-nginx"}
+BUILD_VERSION=${5:-"$(date +"%s")"}
+
+echo "START Update frontend docker image [ BUILD_ONLY = ${BUILD_ONLY} ] @ "`date`
 
 cd frontend/
-aws ecr get-login-password --region eu-central-1 | sudo docker login --username AWS --password-stdin 884088487044.dkr.ecr.eu-central-1.amazonaws.com
-sudo docker build --no-cache -t eu-central-1-test-plizi-front-nginx:latest -f docker/front-nginx/Dockerfile .
-sudo docker tag eu-central-1-test-plizi-front-nginx:latest 884088487044.dkr.ecr.eu-central-1.amazonaws.com/eu-central-1-test-plizi-front-nginx:latest
-sudo docker push 884088487044.dkr.ecr.eu-central-1.amazonaws.com/eu-central-1-test-plizi-front-nginx:latest
+if [ -z $BUILD_ONLY ]; then
+	aws ecr get-login-password --region eu-central-1 | sudo docker login --username AWS --password-stdin 884088487044.dkr.ecr.eu-central-1.amazonaws.com
+fi
+sudo docker build \
+-t "${IMAGE_NAME}:latest" \
+-f ${BUILD_FILE_PATH}/Dockerfile \
+--build-arg VERSION=${BUILD_VERSION} .
+if [ -z $BUILD_ONLY ]; then
+	sudo docker tag eu-central-1-test-plizi-front-nginx:latest 884088487044.dkr.ecr.eu-central-1.amazonaws.com/eu-central-1-test-plizi-front-nginx:latest
+	sudo docker push 884088487044.dkr.ecr.eu-central-1.amazonaws.com/eu-central-1-test-plizi-front-nginx:latest
+fi
 
 echo "FINISH Update frontend docker image @ "`date`
 
