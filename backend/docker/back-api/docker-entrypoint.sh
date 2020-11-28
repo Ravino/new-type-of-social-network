2a6fa3be17a81ddad213ea2431d898bbafac3640
Dmitry Korzhov <dkorzhov1c@gmail.com>
2020-11-28 19:15:55 +0300

    Add line start http backend server

--- backend/docker/back-api/docker-entrypoint.sh
+++ backend/docker/back-api/docker-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 #exec php artisan migrate --force
-exec php artisan db:seed --force && exec php artisan swoole:http start
+#exec php artisan db:seed --force 
+exec php artisan swoole:http start
 
