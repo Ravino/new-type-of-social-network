b6f5b3f26892e3fae231874b21b4ea3f66eeae4e
Dmitry Korzhov <dkorzhov1c@gmail.com>
2020-11-28 19:02:29 +0300

    Add line migrate data to MySQL

--- backend/docker/back-api/docker-entrypoint.sh
+++ backend/docker/back-api/docker-entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 #exec php artisan migrate --force
-#exec php artisan db:seed --force
-
-exec php artisan swoole:http start
+exec php artisan db:seed --force && exec php artisan swoole:http start
 
