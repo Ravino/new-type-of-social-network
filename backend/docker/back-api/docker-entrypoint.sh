6723f2c30588d333e308a4b7fa181747740c775c
Dmitry Korzhov <dkorzhov1c@gmail.com>
2020-11-28 16:34:35 +0300

    Del line create structure database

--- backend/docker/back-api/docker-entrypoint.sh
+++ backend/docker/back-api/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-exec php artisan migrate --force
-exec php artisan db:seed --force
-
+#exec php artisan migrate --force
+#exec php artisan db:seed --force
+exec echo $(ls -la)
 exec php artisan swoole:http start
 
