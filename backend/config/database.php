3222661ff743d6f1f06e71e5961600feeb934a97
Dmitry Korzhov <dkorzhov1c@gmail.com>
2020-12-02 15:58:40 +0300

    Change to redis cache v2

--- backend/config/database.php
+++ backend/config/database.php
@@ -153,7 +153,7 @@ return [
             'parameters' => ['password' => env('REDIS_PASSWORD', null)],
         ],
 
-        'cache' => [
+        'clustered' => [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
