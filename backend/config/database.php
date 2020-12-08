a2310291620528e079985dd55adad4e494732700
Dmitry Korzhov <dkorzhov1c@gmail.com>
2020-12-08 14:42:29 +0300

    Change phpredis on predis

--- backend/config/database.php
+++ backend/config/database.php
@@ -140,7 +140,7 @@ return [
     */
 
     'redis' => [
-        'client' => env('REDIS_CLIENT', 'phpredis'),
+        'client' => env('REDIS_CLIENT', 'predis'),
         'cluster' => false,
         'default' => [
             'host' => env('REDIS_HOST', 'localhost'),
