22e5e244d4ea116c8762b39326bf98af35374480
Dmitry Korzhov <dkorzhov1c@gmail.com>
2020-12-13 15:51:09 +0300

    Add worker restart

--- backend/docker/back-queue-worker/docker-entrypoint.sh
+++ backend/docker/back-queue-worker/docker-entrypoint.sh
@@ -3,6 +3,6 @@
 #exec php artisan queue:work redis --queue=high,default
 exec php artisan queue:work --queue=redis2,redis
 #exec php artisan queue:work --quiet --queue=redis2,redis
-#echo "Starting..."
-#pgrep -fl supervisor
-#exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
+echo "Starting..."
+pgrep -fl supervisor
+exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
