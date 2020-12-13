cb55392d4369340f568707f12948d5b8d0c79878
Dmitry Korzhov <dkorzhov1c@gmail.com>
2020-12-13 13:49:08 +0300

    Add queue redis

--- backend/docker/back-queue-worker/docker-entrypoint.sh
+++ backend/docker/back-queue-worker/docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-exec php artisan queue:work redis --queue=high,default
+#exec php artisan queue:work redis --queue=high,default
 #exec php artisan queue:work --queue=redis2,redis
-#exec php artisan queue:work --quiet --queue=redis2,redis
+exec php artisan queue:work --quiet --queue=redis2,redis
 #echo "Starting..."
 #pgrep -fl supervisor
 #exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
