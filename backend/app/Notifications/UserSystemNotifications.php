f1315eab8a0b5a23b5a201cf8597963972e31114
Dmitry Korzhov <dkorzhov1c@gmail.com>
2020-12-20 18:25:32 +0300

    Try push redis v5

--- backend/app/Notifications/UserSystemNotifications.php
+++ backend/app/Notifications/UserSystemNotifications.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class UserSystemNotifications extends Notification
+class UserSystemNotifications extends Notification implements ShouldQueue
 {
 
     private $details;
