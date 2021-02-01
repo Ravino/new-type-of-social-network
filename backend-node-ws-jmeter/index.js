4e8532bf09bbc7d6c12372421773fcbd5f49ac36
Ravino Doul <evgeniy@nekrasov.pw>
2021-02-01 22:19:09 +1000

    Added env

---
+++ backend-node-ws-jmeter/index.js
@@ -0,0 +1,60 @@
+"use strict";
+
+
+const envSocketPort = global.process.env.SOCKET_PORT;
+const envRedisHost = global.process.env.REDIS_HOST;
+const envRedisPort = global.process.env.REDIS_PORT;
+const envRoomChat = global.process.env.ROOM_CHAT;
+
+
+const app = require("express")();
+const http = require("http");
+const SocketIoEmitter = require ("socket.io-emitter");
+const Ioredis = require ("ioredis");
+const WebSocket = require ("ws");
+
+
+const ioredis = new Ioredis(`redis://${ envRedisHost }:${ envRedisPort }`);
+
+
+const httpServer = http.Server(app);
+const socketIoEmitter = SocketIoEmitter (`redis://${ envRedisHost }:${ envRedisPort }`);
+const webSocketServer = new WebSocket.Server({server: httpServer});
+
+
+webSocketServer.on("connection", (socket) => {
+
+  console.log("client connect");
+  socket.send("connect");
+
+
+  socket.on("message", (data) => {
+
+    try {
+      data = JSON.parse(data);
+    }
+    catch(err) {
+      console.log(err);
+      socket.send("Error data");
+      return undefined;
+    }
+
+
+    console.log(data);
+//    socket.send("message delivered");
+    socketIoEmitter.to(envRoomChat + data.idChat).emit("chat-message", data);
+    return undefined;
+  });
+
+
+  return undefined;
+});
+
+
+app.use("/check", (req, res) => {
+  res.sendStatus(200);
+  return undefined;
+});
+
+
+httpServer.listen(envSocketPort);
