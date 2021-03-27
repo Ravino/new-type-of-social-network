80d94346d793655bcbf8376057c745ec6d4b6915
Ravino Doul <evgeniy@nekrasov.pw>
2021-03-27 18:40:25 +1000

    Added config passport

--- backend/registration-authorization/src/config/passport.ts
+++ backend/registration-authorization/src/config/passport.ts
@@ -1,11 +1,20 @@
 import { Application } from 'express';
 import passport from 'passport';
-import emailStrategy from '../passportStrategy/emailStrategy';
-
-
-passport.use('email', emailStrategy);
+import { emailStrategy } from '../passportStrategy/emailStrategy';
+import { vkontakteStrategy } from '../passportStrategy/vkontakteStrategy';
 
 
 export function passportInitialization(server: Application) {
   server.use(passport.initialize());
 }
+
+
+export const configStrategyVkontakte = {
+  clientID: <string>global.process.env.VKONTAKTE_CLIENT_ID,
+  clientSecret: <string>global.process.env.VKONTAKTE_CLIENT_SECRET,
+  callbackURL: <string>global.process.env.VKONTAKTE_CALLBACK_URL
+}
+
+
+passport.use('email', emailStrategy());
+passport.use('vkontakte', vkontakteStrategy());
