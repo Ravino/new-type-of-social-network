40b744e1bb598f5ec2e8ae060371a08950b779f7
Ravino Doul <evgeniy@nekrasov.pw>
2021-03-27 18:39:57 +1000

    Added  initialize env

--- backend/registration-authorization/src/application.ts
+++ backend/registration-authorization/src/application.ts
@@ -1,9 +1,11 @@
 import { server } from './config/server';
+import { environmentInitialization } from './config/environment';
 import { bodyParserInitialization } from './config/bodyParser';
 import { passportInitialization } from './config/passport';
 import { routerInitialization } from './config/router';
 
 
+environmentInitialization();
 bodyParserInitialization(server);
 passportInitialization(server);
 routerInitialization(server);
