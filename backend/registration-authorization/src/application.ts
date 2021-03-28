41eadeb0d6ee59b3369e32b0fb4b16088393742a
Ravino Doul <evgeniy@nekrasov.pw>
2021-03-28 23:39:48 +1000

    Added connect to Tarantool

--- backend/registration-authorization/src/application.ts
+++ backend/registration-authorization/src/application.ts
@@ -1,3 +1,5 @@
+import TarantoolDriver from 'tarantool-driver';
+import { tarantool } from './config/tarantool';
 import { server } from './config/server';
 import { environmentInitialization } from './config/environment';
 import { bodyParserInitialization } from './config/bodyParser';
@@ -10,7 +12,19 @@ bodyParserInitialization(server);
 passportInitialization(server);
 routerInitialization(server);
 
+tarantool.on('connect', () => {
 
-server.listen(3000, () => {
-  console.log(`Application started on port 3000...`);
+  server.listen(3000, () => {
+    console.log(`Application started on port 3000...`);
+    return undefined;
+  });
+
+
+  return undefined;
+});
+
+
+tarantool.on('reconnecting', () => {
+  console.log('Reconnect to tarantool');
+  return undefined;
 });
