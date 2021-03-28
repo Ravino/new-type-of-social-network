ba08f95d610c02d094016e5968eb7f6f2bb64bd0
Ravino Doul <evgeniy@nekrasov.pw>
2021-03-28 21:46:32 +1000

    Added new rules for gitignore

--- backend/registration-authorization/lib/service/authorization.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Authorization = void 0;
-const abstractRegistrationAuthorization_1 = require("./abstractRegistrationAuthorization");
-class Authorization extends abstractRegistrationAuthorization_1.AbstractRegistrationAuthorization {
-}
-exports.Authorization = Authorization;
-//# sourceMappingURL=authorization.js.map
