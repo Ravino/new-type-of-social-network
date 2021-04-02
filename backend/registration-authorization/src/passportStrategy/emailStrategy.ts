94719df72c8dc33cad6b6c2be911ff820d7b148b
Ravino Doul <evgeniy@nekrasov.pw>
2021-04-02 18:00:37 +1000

    Updated logic auth

--- backend/registration-authorization/src/passportStrategy/emailStrategy.ts
+++ backend/registration-authorization/src/passportStrategy/emailStrategy.ts
@@ -1,22 +1,61 @@
 import { Container } from 'typescript-ioc';
+import escapeHtml from 'escape-html';
+import trim from 'trim';
 import { AuthorizationService } from '../service/authorizationService';
+import { UserService } from '../service/userService';
 import { Strategy } from 'passport-local';
 
 
-export const emailStrategy = () => new Strategy((email: string, password: string, done: any) => {
+export const emailStrategy = () => new Strategy(async (email: string, password: string, done: any) => {
+
+  email = escapeHtml(email || '');
+  password = escapeHtml(password);
+
+
+  email = trim(email);
+  password = trim(password);
+
 
   if(!Container.get(AuthorizationService).validateEmail(email)) {
-    done(null, false);
+    done(null, false, { message: 'invalidEmail'});
     return undefined;
   }
 
 
   if(!Container.get(AuthorizationService).validatePassword(password)) {
-    done(null, false);
+    done(null, false, { message: 'invalidPassword'});
     return undefined;
   }
 
 
-  done(null, true);
+  let existUser: any;
+  try {
+    existUser = await Container.get(UserService).getByEmail(email);
+  }
+  catch(err) {
+    console.log(err);
+    done(null, false, { message: 'notSuccess'});
+    return undefined;
+  }
+
+
+  if(!existUser) {
+    done(null, false, { message: 'notExistUser'});
+    return undefined;
+  }
+
+
+  let statusVerifyPassword: boolean = false;
+  try {
+    statusVerifyPassword = await Container.get(AuthorizationService).verifyPassword(password, existUser.PASSWORD);
+  }
+  catch(err) {
+    console.log(err);
+    done(null, false, { message: 'notSuccess'});
+    return undefined;
+  }
+
+
+  done(null, true, {message: 'success'});
   return undefined;
 })
