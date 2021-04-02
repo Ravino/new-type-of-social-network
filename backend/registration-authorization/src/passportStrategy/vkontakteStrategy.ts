2a6a5d9000d7b136d4942354aa216dab56e06604
Ravino Doul <evgeniy@nekrasov.pw>
2021-04-03 05:43:30 +1000

    Added logic for vk auth

--- backend/registration-authorization/src/passportStrategy/vkontakteStrategy.ts
+++ backend/registration-authorization/src/passportStrategy/vkontakteStrategy.ts
@@ -1,16 +1,63 @@
 import { Container } from 'typescript-ioc';
 import { configStrategyVkontakte } from '../config/passport';
+import { UserService } from '../service/userService';
 import { AuthorizationService } from '../service/authorizationService';
 import { Strategy } from 'passport-vkontakte';
 
 
-export const vkontakteStrategy = () => new Strategy(configStrategyVkontakte, (accessToken: string, refreshToken: string, params: any, profile: any, done: any) => {
+export const vkontakteStrategy = () => new Strategy(configStrategyVkontakte, async (accessToken: string, refreshToken: string, params: any, profile: any, done: any) => {
 
-/*
-  console.log(accessToken);
-  console.log(refreshToken);
-  console.log(params);
-  console.log(profile);
-*/
-  done(null, true);
-})
+  let existUser: any;
+  try {
+    existUser = await Container.get(UserService).getByNameField('vkontakte_profile_id', params.user_id);
+  }
+  catch(err) {
+    console.log(err);
+    done(null, false, { message: 'notSuccess'});
+    return undefined;
+  }
+
+
+  if(!existUser) {
+    const profileName = profile.name;
+    try {
+      await Container.get(UserService).create(profileName.givenName, profileName.familyName, '', true, null, params.user_id);
+    }
+    catch(err) {
+      console.log(err);
+      done(null, false, { message: 'notSuccess'});
+      return undefined;
+    }
+  }
+
+
+  try {
+    existUser = await Container.get(UserService).getByNameField('vkontakte_profile_id', params.user_id);
+  }
+  catch(err) {
+    console.log(err);
+    done(null, false, { message: 'notSuccess'});
+    return undefined;
+  }
+
+
+  if(!existUser) {
+    done(null, false, { message: 'notSuccess'});
+    return undefined;
+  }
+
+
+  let pairToken: any;
+  try {
+    pairToken = await Container.get(AuthorizationService).getPairToken(existUser);
+  }
+  catch(err) {
+    console.log(err);
+    done(null, false, { message: 'notSuccess'});
+    return undefined;
+  }
+
+
+  done(null, pairToken, { message: 'success'});
+  return undefined;
+});
