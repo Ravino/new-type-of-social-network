3db4e1f21db9a021f8a91b19ddf81174f0064fc5
Ravino Doul <evgeniy@nekrasov.pw>
2021-04-06 04:04:44 +1000

    Added new route recovery

--- backend/registration-authorization/src/config/router.ts
+++ backend/registration-authorization/src/config/router.ts
@@ -3,6 +3,7 @@ import { Application } from 'express';
 import { Router } from 'express';
 import { SigninRouter } from '../router/signinRouter';
 import { SessionRouter } from '../router/sessionRouter';
+import { RecoveryRouter } from '../router/recoveryRouter';
 
 
 const router: Router = Router();
@@ -15,6 +16,11 @@ router.use('/signin',
 
 router.use('/session',
   Container.get(SessionRouter).handler()
+);
+
+
+router.use('/recovery',
+  Container.get(RecoveryRouter).handler()
 );
 
 
