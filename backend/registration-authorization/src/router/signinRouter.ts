8ad80078f6dd923cec1f01b63f23ab922b838e04
Ravino Doul <evgeniy@nekrasov.pw>
2021-04-09 02:26:43 +1000

    Added middlewares

--- backend/registration-authorization/src/router/signinRouter.ts
+++ backend/registration-authorization/src/router/signinRouter.ts
@@ -1,12 +1,18 @@
 import { Container } from 'typescript-ioc';
 import { Router } from 'express';
-import { Request } from 'express';
-import { Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { EmailRouter } from './emailRouter';
 import { VkontakteRouter } from './vkontakteRouter';
+import { AccessMiddleware } from '../middleware/accessMiddleware';
 
 
 export class SigninRouter {
+
+  public middleware(req: Request, res: Response, next: NextFunction): any {
+    Container.get(AccessMiddleware).checkExistSession(req, res, next);
+    return 
+  }
+
 
   public handler(): Router {
 
