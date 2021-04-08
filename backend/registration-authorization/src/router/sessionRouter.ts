291bbab161dcea4521077e36002f3047d139ad40
Ravino Doul <evgeniy@nekrasov.pw>
2021-04-09 02:26:50 +1000

    Added middlewares

--- backend/registration-authorization/src/router/sessionRouter.ts
+++ backend/registration-authorization/src/router/sessionRouter.ts
@@ -1,12 +1,18 @@
 import { Container } from 'typescript-ioc';
 import { Router } from 'express';
-import { Request } from 'express';
-import { Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { SessionUpdateRouter } from './sessionUpdateRouter';
 import { SessionDestroyRouter } from './sessionDestroyRouter';
+import { AccessMiddleware } from '../middleware/accessMiddleware';
 
 
 export class SessionRouter {
+
+  public middleware(req: Request, res: Response, next: NextFunction): any {
+    Container.get(AccessMiddleware).checkNotExistSession(req, res, next);
+    return 
+  }
+
 
   public handler(): Router {
 
