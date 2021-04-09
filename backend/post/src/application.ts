d2929771795c6ec997147435cef00ce153358e4b
Ravino Doul <evgeniy@nekrasov.pw>
2021-04-10 09:20:56 +1000

    Post/added entrypoint file

---
+++ backend/post/src/application.ts
@@ -0,0 +1,24 @@
+import { tarantool } from './config/tarantool';
+import { server } from './config/server';
+import { environmentInitialization } from './config/environment';
+
+
+environmentInitialization();
+
+
+tarantool.on('connect', () => {
+
+  server.listen(3000, () => {
+    console.log(`Application started on port 3000...`);
+    return undefined;
+  });
+
+
+  return undefined;
+});
+
+
+tarantool.on('reconnecting', () => {
+  console.log('Reconnect to tarantool');
+  return undefined;
+});
