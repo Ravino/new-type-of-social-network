f97752de90c03444865570eaa192bfab87c03539
Ravino Doul <evgeniy@nekrasov.pw>
2021-04-11 04:37:13 +1000

    Post/added configure appollo server with resolvers hi level

--- backend/post/src/config/apolloPost.ts
+++ backend/post/src/config/apolloPost.ts
@@ -3,6 +3,8 @@ import {readFileSync} from "fs";
 import {ApolloServer, gql} from "apollo-server-express";
 import {Container} from "typescript-ioc";
 import { Application } from 'express';
+import {PostQuery} from '../graphql/postQuery';
+import {PostRequestQuery} from '../graphql/postRequestQuery';
 
 
 const pathSchema: string = path.join(__dirname, "../graphql/post.graphql");
@@ -10,7 +12,20 @@ const schemaContent: string = readFileSync(pathSchema, "utf8");
 const typeDefs = gql`${ schemaContent }`;
 
 
-const resolvers = {};
+const resolvers = {
+
+  Query: {
+    requestQuery: () => Container.get(PostQuery).requestQuery
+  },
+
+
+  RequestQuery: {
+    get: (parent: PostRequestQuery, args: {postId: string}) => parent.get(args.postId),
+    requests: (parent: PostRequestQuery, args: {size: number, textSearch: string}) => parent.requests(args.size, args.textSearch),
+    select: (parent: PostRequestQuery, args: {cursor: string, offset: number}) => parent.select(args.cursor, args.offset)
+  }
+};
+
 
 
 const apolloServer: ApolloServer = new ApolloServer({ typeDefs, resolvers });
