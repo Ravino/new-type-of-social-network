897fc7bf0a8ea72ba1870d28987ee22a5fa4cb99
Ravino Doul <evgeniy@nekrasov.pw>
2021-04-11 04:39:55 +1000

    Post/added view model for render to client for post

---
+++ backend/post/src/view/postRequestView.ts
@@ -0,0 +1,8 @@
+export class PostRequestView {
+  public postId?: number;
+  public userId?: number;
+  public displayName?: string;
+  public body?: string;
+  public createdAt?: Date;
+  public updatedAt?: Date;
+}
