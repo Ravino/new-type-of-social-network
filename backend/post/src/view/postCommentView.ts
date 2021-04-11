192b341a041d51fd012e2cf18960acb1fd699b39
Ravino Doul <evgeniy@nekrasov.pw>
2021-04-12 09:39:28 +1000

    Post/added view for comment

---
+++ backend/post/src/view/postCommentView.ts
@@ -0,0 +1,9 @@
+export class PostCommentView {
+  public commentId?: number;
+  public postId?: number;
+  public userId?: number;
+  public displayName?: string;
+  public body?: string;
+  public createdAt?: Date;
+  public updatedAt?: Date;
+}
