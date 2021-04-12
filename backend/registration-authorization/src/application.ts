7b4c959244ff26bda6fe0165e245bc893520f8e5
Ravino Doul <evgeniy@nekrasov.pw>
2021-04-12 10:14:14 +1000

    Remove imports and configures

--- backend/registration-authorization/src/application.ts
+++ backend/registration-authorization/src/application.ts
@@ -1,19 +1,6 @@
 import { tarantool } from './config/tarantool';
 import { server } from './config/server';
-import { environmentInitialization } from './config/environment';
-import { ipInfoInitialization } from './config/ipInfo';
-import { cookieParserInitialization } from './config/cookieParser';
-import { bodyParserInitialization } from './config/bodyParser';
-import { passportInitialization } from './config/passport';
-import { routerInitialization } from './config/router';
 
-
-environmentInitialization();
-ipInfoInitialization(server);
-cookieParserInitialization(server);
-bodyParserInitialization(server);
-passportInitialization(server);
-routerInitialization(server);
 
 tarantool.on('connect', () => {
 
