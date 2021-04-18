3a78f7a92f91fa1132cfd303451ce5b56758d972
Ravino Doul <evgeniy@nekrasov.pw>
2021-04-18 17:26:55 +1000

    Gateway/added view for post

---
+++ backend/gateway/src/view/postView.ts
@@ -0,0 +1,8 @@
+export class PostView {
+  public postId?: number;
+  public authorId?: number;
+  public displayName?: string;
+  public body?: string;
+  public createdAt?: Date;
+  public updatedAt?: Date;
+}
