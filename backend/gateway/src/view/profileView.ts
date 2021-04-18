3a387b02799fff0e15ffbdffc47d50e07f3329c7
Ravino Doul <evgeniy@nekrasov.pw>
2021-04-18 17:26:36 +1000

    Gateway/added view for profile

---
+++ backend/gateway/src/view/profileView.ts
@@ -0,0 +1,10 @@
+export class ProfileView {
+  public profileId?: number;
+  public firstname?: string;
+  public lastname?: string;
+  public displayName?: string;
+  public avatar?: string;
+  public country?: string;
+  public city?: string;
+  public age?: number;
+}
