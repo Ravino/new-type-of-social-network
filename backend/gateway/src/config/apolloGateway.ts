d6f2d73d95b9271d818b919f7898ca8fa7520fad
Ravino Doul <evgeniy@nekrasov.pw>
2021-04-19 04:40:15 +1000

    Gateway/added import and resolvers for likes and shares

--- backend/gateway/src/config/apolloGateway.ts
+++ backend/gateway/src/config/apolloGateway.ts
@@ -3,14 +3,23 @@ import {readFileSync} from "fs";
 import {ApolloServer, gql} from "apollo-server-express";
 import {Container} from "typescript-ioc";
 import { Application } from 'express';
+
+
 import {QueryResolver} from '../resolver/queryResolver';
 import {RequestProfileResolver} from '../resolver/requestProfileResolver';
 import {RequestPostResolver} from '../resolver/requestPostResolver';
 import {RequestCommentResolver} from '../resolver/requestCommentResolver';
+import {RequestLikeResolver} from '../resolver/requestLikeResolver';
+import {RequestShareResolver} from '../resolver/requestShareResolver';
+
+
 import {ProfileView} from '../view/profileView';
 import {PostView} from '../view/postView';
+import {CommentView} from '../view/commentView';
 import {PostListView} from '../view/postListView';
 import {CommentListView} from '../view/commentListView';
+import {LikeListView} from '../view/likeListView';
+import {ShareListView} from '../view/shareListView';
 
 
 const pathSchema: string = path.join(__dirname, "../graphql/main.graphql");
@@ -37,7 +46,15 @@ const resolvers = {
 
 
   Post: {
+    likes: (parent: PostView) => parent,
+    shares: (parent: PostView) => parent,
     comments: (parent: PostView) => parent
+  },
+
+
+  Comment: {
+    likes: (parent: CommentView) => parent,
+    shares: (parent: CommentView) => parent
   },
 
 
@@ -51,6 +68,16 @@ const resolvers = {
   },
 
 
+  RequestLike: {
+    getList: () => Container.get(RequestLikeResolver).getList()
+  },
+
+
+  RequestShare: {
+    getList: () => Container.get(RequestShareResolver).getList()
+  },
+
+
   PostList: {
     count: (parent: PostListView) => parent.count(),
     items: (parent: PostListView) => parent.items()
@@ -60,6 +87,18 @@ const resolvers = {
   CommentList: {
     count: (parent: CommentListView) => parent.count(),
     items: (parent: CommentListView) => parent.items()
+  },
+
+
+  LikeList: {
+    count: (parent: LikeListView) => parent.count(),
+    items: (parent: LikeListView) => parent.items()
+  },
+
+
+  ShareList: {
+    count: (parent: ShareListView) => parent.count(),
+    items: (parent: ShareListView) => parent.items()
   }
 };
 
