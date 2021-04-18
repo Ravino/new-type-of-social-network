4871474506c229c7283e298dc6f13e976e0f17bf
Ravino Doul <evgeniy@nekrasov.pw>
2021-04-18 21:25:26 +1000

    Gateway/added entity for profile

---
+++ backend/gateway/src/model/profileEntity.ts
@@ -0,0 +1,15 @@
+export enum ProfileGenderState {
+  ABSENT = 'ABSENT',
+  FEMALE = 'FEMALE',
+  MALE = 'MALE'
+}
+
+
+export enum ProfileMaritalState {
+  NOT_MARRIED = 'NOT_MARRIED',
+  MARRIED = 'MARRIED',
+  ACTIVE_SEARCH = 'ACTIVE_SEARCH',
+  RELATIONSHIP = 'RELATIONSHIP',
+  AVAILABLE = 'AVAILABLE',
+  ABSENT = 'ABSENT'
+}
