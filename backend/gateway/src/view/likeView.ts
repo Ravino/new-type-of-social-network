3a40a685d37dcfb7eea8bb97a0ecc8781985fe78
Ravino Doul <evgeniy@nekrasov.pw>
2021-04-19 04:40:44 +1000

    Gateway/added view for like

---
+++ backend/gateway/src/view/likeView.ts
@@ -0,0 +1,8 @@
+export class LikeView {
+  public likeId?: number;
+  public authorId?: number;
+  public displayName?: string;
+  public avatar?: string;
+  public createdAt?: Date;
+  public updatedAt?: Date;
+}
