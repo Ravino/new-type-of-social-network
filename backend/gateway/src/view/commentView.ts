4daeea74121cf00f30cb0085b7ba3cd5509e0cd5
Ravino Doul <evgeniy@nekrasov.pw>
2021-04-18 17:27:10 +1000

    Gateway/added view for comment

---
+++ backend/gateway/src/view/commentView.ts
@@ -0,0 +1,9 @@
+export class CommentView {
+  public commentId?: number;
+  public postId?: number;
+  public authorId?: number;
+  public displayName?: string;
+  public body?: string;
+  public createdAt?: Date;
+  public updatedAt?: Date;
+}
