897b90db789426e03af8a48e114e8462d3d7ef82
Ravino Doul <evgeniy@nekrasov.pw>
2021-04-19 05:36:56 +1000

    Gateway/added view for frend

---
+++ backend/gateway/src/view/frendView.ts
@@ -0,0 +1,8 @@
+export class FrendView {
+  public frendId?: number;
+  public profileId?: number;
+  public displayName?: string;
+  public avatar?: string;
+  public createdAt?: Date;
+  public updatedAt?: Date;
+}
