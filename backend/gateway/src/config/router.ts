6f3ed873cc45d21be932b614d1e4ce7f7f58f42e
Ravino Doul <evgeniy@nekrasov.pw>
2021-04-25 10:24:49 +1000

    Gatewayadded router for secure and rest api

---
+++ backend/gateway/src/config/router.ts
@@ -0,0 +1,25 @@
+import { Container } from 'typescript-ioc';
+import { Application } from 'express';
+import { Router } from 'express';
+import { GraphqlRouter } from '../router/graphqlRouter';
+import { OtherRouter } from '../router/otherRouter';
+
+
+const router: Router = Router();
+
+
+router.use('/graphql',
+  Container.get(GraphqlRouter).middleware
+//  Container.get(OtherRouter).handler()
+);
+
+
+router.use('/*',
+  Container.get(OtherRouter).middleware,
+  Container.get(OtherRouter).handler()
+);
+
+
+export function routerInitialization(server: Application) {
+  server.use(router);
+}
