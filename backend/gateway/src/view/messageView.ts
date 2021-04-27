c6a85134b900b9e74b93735c468ae8499924bcb6
Ravino Doul <evgeniy@nekrasov.pw>
2021-04-28 07:41:40 +1000

    Gateway/added field avatar into view  message

--- backend/gateway/src/view/messageView.ts
+++ backend/gateway/src/view/messageView.ts
@@ -2,6 +2,7 @@ export class MessageView {
   public messageId?: number;
   public senderId?: number;
   public chatId?: number;
+  public avatar?: string;
   public displayName?: string;
   public body?: string;
   public createdAt?: Date;
