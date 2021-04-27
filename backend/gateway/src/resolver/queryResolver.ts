41ef1a9b6fdb6343be100f7b0cef605307f15b03
Ravino Doul <evgeniy@nekrasov.pw>
2021-04-28 03:34:00 +1000

    Gateway/used resolver for requestChat

--- backend/gateway/src/resolver/queryResolver.ts
+++ backend/gateway/src/resolver/queryResolver.ts
@@ -1,10 +1,12 @@
 import {Inject, Singleton} from 'typescript-ioc';
 import {RequestProfileResolver} from './requestProfileResolver';
+import {RequestChatResolver} from './requestChatResolver';
 
 
 @Singleton
 export class QueryResolver {
   public constructor(
-    @Inject public readonly requestProfileResolver: RequestProfileResolver
+    @Inject public readonly requestProfileResolver: RequestProfileResolver,
+    @Inject public readonly requestChatResolver: RequestChatResolver
   ) {}
 }
