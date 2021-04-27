0d1d09ed4408df57f3089d162f8381480bcb680c
Ravino Doul <evgeniy@nekrasov.pw>
2021-04-28 08:11:30 +1000

    Gateway/added inject resolver for request message

--- backend/gateway/src/resolver/queryResolver.ts
+++ backend/gateway/src/resolver/queryResolver.ts
@@ -1,12 +1,14 @@
 import {Inject, Singleton} from 'typescript-ioc';
 import {RequestProfileResolver} from './requestProfileResolver';
 import {RequestChatResolver} from './requestChatResolver';
+import {RequestMessageResolver} from '../resolver/requestMessageResolver';
 
 
 @Singleton
 export class QueryResolver {
   public constructor(
     @Inject public readonly requestProfileResolver: RequestProfileResolver,
-    @Inject public readonly requestChatResolver: RequestChatResolver
+    @Inject public readonly requestChatResolver: RequestChatResolver,
+    @Inject public readonly requestMessageResolver: RequestMessageResolver
   ) {}
 }
