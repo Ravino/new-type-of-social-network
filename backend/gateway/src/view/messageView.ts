3a2a61c9d1711dba743aef07a6ae47861c59d33c
Ravino Doul <evgeniy@nekrasov.pw>
2021-04-28 03:05:52 +1000

    Gateway/added view messageView

---
+++ backend/gateway/src/view/messageView.ts
@@ -0,0 +1,9 @@
+export class MessageView {
+  public messageId?: number;
+  public senderId?: number;
+  public chatId?: number;
+  public displayName?: string;
+  public body?: string;
+  public createdAt?: Date;
+  public updatedAt?: Date;
+}
