76306aba562b5c10e865273d7ec0189159e5105c
Ravino Doul <evgeniy@nekrasov.pw>
2021-05-01 06:14:00 +1000

    Chat/added  initialization socket.io

--- backend/chat/src/config/server.ts
+++ backend/chat/src/config/server.ts
@@ -3,6 +3,7 @@ import express, {Application} from 'express';
 import { cookieParserInitialization } from './cookieParser';
 import { bodyParserInitialization } from './bodyParser';
 import {routerInitialization} from './router';
+import {socketioInitialization} from './socketio';
 
 
 const app: Application = express();
@@ -12,3 +13,4 @@ export const server: Server = new Server(app);
 cookieParserInitialization(app);
 bodyParserInitialization(app);
 routerInitialization(app);
+socketioInitialization(server);
