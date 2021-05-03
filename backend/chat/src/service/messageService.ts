df97eb3cd66b015dc02c03a8e3c21dc04c0eb765
Ravino Doul <evgeniy@nekrasov.pw>
2021-05-03 19:52:21 +1000

    Chat/added template for service of message

---
+++ backend/chat/src/service/messageService.ts
@@ -0,0 +1,20 @@
+import {tarantool} from '../config/tarantool';
+import {v4 as uuidV4} from 'uuid';
+
+
+export class MessageService{
+
+  public constructor() {}
+
+
+  public async get(): Promise<any> {}
+
+
+  public async save(): Promise<any> {}
+
+
+  public async update(): Promise<any> {}
+
+
+  public async del(): Promise<any> {}
+}
