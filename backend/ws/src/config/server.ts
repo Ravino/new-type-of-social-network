822c16e99cb5b920a9979f5badb505f70632a8bd
Ravino Doul <evgeniy@nekrasov.pw>
2021-05-05 00:30:34 +1000

    ws/added template middleware

--- backend/ws/src/config/server.ts
+++ backend/ws/src/config/server.ts
@@ -1,4 +1,8 @@
-import {Server as SocketioServer} from 'socket.io';
+import {
+  Socket as SocketioSocket,
+  Server as SocketioServer
+} from 'socket.io';
+
 import {Server as HttpServer} from 'http';
 import express, {Application} from 'express';
 import {socketioConfig} from './socketio';
@@ -7,3 +11,13 @@ import {socketioConfig} from './socketio';
 const app: Application = express();
 export const httpServer: HttpServer = new HttpServer(app);
 const socketioServer: SocketioServer = new SocketioServer(httpServer, socketioConfig);
+
+
+socketioServer.use((socket: SocketioSocket, next: any) => {
+  next();
+});
+
+
+socketioServer.on('connect', (socketioSocket: SocketioSocket) => {
+  console.log("connect");
+});
