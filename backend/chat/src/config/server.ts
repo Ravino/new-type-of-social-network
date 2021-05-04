ab9906399063d0195d513cb02c6f79bc7788aaf2
Ravino Doul <evgeniy@nekrasov.pw>
2021-05-04 14:53:16 +1000

    Chat/remove initialization socketio

--- backend/chat/src/config/server.ts
+++ backend/chat/src/config/server.ts
@@ -3,7 +3,6 @@ import express, {Application} from 'express';
 import { cookieParserInitialization } from './cookieParser';
 import { bodyParserInitialization } from './bodyParser';
 import {routerInitialization} from './router';
-import {socketioInitialization} from './socketio';
 
 
 const app: Application = express();
@@ -13,4 +12,3 @@ export const server: Server = new Server(app);
 cookieParserInitialization(app);
 bodyParserInitialization(app);
 routerInitialization(app);
-socketioInitialization(server);
