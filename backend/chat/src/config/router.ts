fe2d186d0749ce5c5eddcc9a56c7260eb0fc2412
Ravino Doul <evgeniy@nekrasov.pw>
2021-05-04 22:25:08 +1000

    Chat/added stable template

--- backend/chat/src/config/router.ts
+++ backend/chat/src/config/router.ts
@@ -1,9 +1,15 @@
 import { Container } from 'typescript-ioc';
 import { Application } from 'express';
 import { Router } from 'express';
+import {OtherRouter} from '../router/otherRouter';
 
 
 const router: Router = Router();
+
+
+router.use('/*',
+  Container.get(OtherRouter).middleware
+);
 
 
 export function routerInitialization(server: Application) {
