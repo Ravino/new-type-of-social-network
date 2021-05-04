f27243c55c9bdae5ee98fdbcca6c214981ed5ce1
Ravino Doul <evgeniy@nekrasov.pw>
2021-05-04 14:56:07 +1000

    Chat/removed handler path

--- backend/chat/src/config/router.ts
+++ backend/chat/src/config/router.ts
@@ -1,16 +1,9 @@
 import { Container } from 'typescript-ioc';
 import { Application } from 'express';
 import { Router } from 'express';
-import { WebsocketRouter } from '../router/websocketRouter';
 
 
 const router: Router = Router();
-
-
-router.use('/ws',
-  Container.get(WebsocketRouter).middleware
-//  Container.get(GraphqlRouter).handler()
-);
 
 
 export function routerInitialization(server: Application) {
