89758ec1b2ed2803167c87eae55055ecc7e6b8e3
Ravino Doul <evgeniy@nekrasov.pw>
2021-05-05 03:48:24 +1000

    WS/update config parser body

--- backend/ws/src/config/bodyParser.ts
+++ backend/ws/src/config/bodyParser.ts
@@ -1,9 +1,24 @@
-import { Application } from 'express';
+import {Socket, Server} from 'socket.io';
+import {Request, Response} from 'express';
 import bodyParser from 'body-parser';
 
 
-export function bodyParserInitialization(server: Application) {
-  server.use(bodyParser.json());
-  server.use(bodyParser.urlencoded({ extended: true }));
+function wrapperOne(socket: Socket, next: any) {
+  const middleware = bodyParser.json();
+  middleware(<Request>socket.request, <Response>{}, next);
+  return undefined;
+}
+
+
+function wrapperTwo(socket: Socket, next: any) {
+  const middleware = bodyParser.urlencoded({extended: true});
+  middleware(<Request>socket.request, <Response>{}, next);
+  return undefined;
+}
+
+
+export function bodyParserInitialization(server: Server) {
+  server.use(wrapperOne);
+  server.use(wrapperTwo);
   return undefined;
 }
