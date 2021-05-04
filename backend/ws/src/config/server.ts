ca7578629f9965d2e221ca74cf1cd5f8b2013b0f
Ravino Doul <evgeniy@nekrasov.pw>
2021-05-05 03:48:54 +1000

    WS/update config server

--- backend/ws/src/config/server.ts
+++ backend/ws/src/config/server.ts
@@ -6,6 +6,9 @@ import {
 import {Server as HttpServer} from 'http';
 import express, {Application} from 'express';
 import {socketioConfig} from './socketio';
+import {cookieParserInitialization} from './cookieParser';
+import {bodyParserInitialization} from './bodyParser';
+import {preWorkMessage, postWorkMessage} from '../handler/message';
 
 
 const app: Application = express();
@@ -13,11 +16,30 @@ export const httpServer: HttpServer = new HttpServer(app);
 const socketioServer: SocketioServer = new SocketioServer(httpServer, socketioConfig);
 
 
-socketioServer.use((socket: SocketioSocket, next: any) => {
-  next();
-});
+cookieParserInitialization(socketioServer);
+bodyParserInitialization(socketioServer);
 
 
 socketioServer.on('connect', (socketioSocket: SocketioSocket) => {
-  console.log("connect");
+
+  socketioSocket.on('room-join', (data: any) => {
+    const chatId: string = data.chatId;
+    socketioSocket.join(`room-${chatId}`);
+    return undefined;
+  });
+
+
+  socketioSocket.on('room-message', (data: any) => {
+
+    const preMessage: any = preWorkMessage(data);
+    const postMessage: any = postWorkMessage(preMessage);
+    const chatId: string = preMessage.chatId;
+
+
+    socketioSocket.in(`room-${chatId}`).emit('room-message', postMessage);
+    return undefined;
+  });
+
+
+  return undefined;
 });
