017a91c8042709160262b61b2c99471c3cfb1d5a
Ravino Doul <evgeniy@nekrasov.pw>
2021-05-06 23:48:54 +1000

    Chat/change table chat_messages

--- backend/chat/tarantool/structure/space_chat_messages.lua
+++ backend/chat/tarantool/structure/space_chat_messages.lua
@@ -1,2 +1,2 @@
 \set language sql
-CREATE TABLE messages (message_id UNSIGNED UNIQUE NOT NULL primary key autoincrement, chat_id UNSIGNED NOT NULL, sender_id UNSIGNED not null, created_at UNSIGNED NOT NULL, updated_at UNSIGNED NOT NULL, body TEXT NOT NULL, uuid text NOT NULL, confirmed BOOLEAN, vkontakte_profile_id UNSIGNED UNIQUE)
+CREATE TABLE chat_messages (chat_message_id UNSIGNED UNIQUE NOT NULL primary key autoincrement, chat_id UNSIGNED NOT NULL, sender_id UNSIGNED not null, created_at UNSIGNED NOT NULL, updated_at UNSIGNED NOT NULL, body TEXT NOT NULL, uuid text NOT NULL, sended BOOLEAN NOT NULL, delivered BOOLEAN NOT NULL, readed BOOLEAN NOT NULL, readed_notification BOOLEAN NOT NULL)
