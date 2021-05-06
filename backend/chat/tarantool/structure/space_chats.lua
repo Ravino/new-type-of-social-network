9c535669d8ccbc24934eb102f3c98c32a6cd3443
Ravino Doul <evgeniy@nekrasov.pw>
2021-05-06 23:48:41 +1000

    Chat/change table chats

--- backend/chat/tarantool/structure/space_chats.lua
+++ backend/chat/tarantool/structure/space_chats.lua
@@ -1,2 +1,2 @@
 \set language sql
-CREATE TABLE chats (chat_id UNSIGNED UNIQUE NOT NULL primary key autoincrement, display_name TEXT NOT NULL, owner_id UNSIGNED NOT NULL, last_message_id UNSIGNED NOT NULL, created_at UNSIGNED NOT NULL, updated_at UNSIGNED NOT NULL, avatar TEXT NOT NULL, uuid TEXT NOT NULL)
+CREATE TABLE chats (chat_id UNSIGNED UNIQUE NOT NULL primary key autoincrement, display_name TEXT NOT NULL, created_at UNSIGNED NOT NULL, updated_at UNSIGNED NOT NULL, avatar TEXT NOT NULL, uuid TEXT NOT NULL, last_message_id UNSIGNED, riveted_message_id UNSIGNED)
